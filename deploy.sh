c64216a24be8fb61d6183472a2df7df006ba0fae
kbigler <kbigler@cisco.com>
2017-11-06 10:47:56 -0800

    few deploy script updates

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Running build/deploy scripts..."
 npm run build
-echo -e "\n--------------------\n Building react-home \n--------------------"
+echo -e "\n--------------------\nBuilding react-home \n--------------------"
 rm index.html
 cp -r build/index.html ./
 rm -rf ./static
@@ -11,4 +11,4 @@ git status
 git add .
 git status
 git commit
-echo -e "\n--------------------\n Complete\nFinish with a:\ngit push \n--------------------\n"
+echo -e "\n--------------------\nComplete\n--------------------\nFinish with a:\ngit push\n"
