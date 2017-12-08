7312c0442bcfb4a4f816b1df839583e68b7a9932
kbigler <kbigler@cisco.com>
2017-12-07 17:40:40 -0800

    working on timeline

--- src/components/features/Timeline.js
+++ src/components/features/Timeline.js
@@ -6,6 +6,18 @@ import * as colors from 'material-ui/styles/colors';
 export const FORMAT = 'MMMM Y';
 export const TIMELINE_TITLE = 'Timeline';
 
+const styles = {
+  off: {
+    display: 'inline-block'
+  },
+  on: {
+    display: 'inline-block',
+    boxShadow: '2px 3px 4px #999',
+    backgroundColor: colors.indigoA700,
+    maxHeight: '100px'
+  }
+};
+
 export const Timeline = props => {
   // sort array by start date
   props.data.sort((a, b) => {
@@ -16,27 +28,26 @@ export const Timeline = props => {
   const beginning = props.data[0].start;
   const total_duration = moment().diff(beginning, 'months');
 
-  const styles = {
-    off: {
-      display: 'inline-block'
-    },
-    on: {
-      backgroundColor: colors.indigoA700,
-      maxHeight: '100px',
-      display: 'inline-block'
-    }
-  };
-
   const WIDTH = 100;
+
+  let added = [];
 
   return (
     <div>
       {props.data.map((job, i) => {
+        // skip if added already
+        if (added.indexOf[i] === -1) {
+          return;
+        }
+
+        // properties
         // company: 'Santa Clara University',
         // location: 'Santa Clara, CA',
         // title: 'Undergraduate Student',
         // start: moment('2011-09-01'),
         // end: moment('2015-06-30')
+
+        // get start and end
         const start = Math.floor(
           job.start.diff(beginning, 'months') / total_duration * WIDTH
         );
@@ -45,14 +56,25 @@ export const Timeline = props => {
         );
         const mid = end - start;
 
+        added.push(i);
+
+        let filler;
+
         for (let j = i + 1; j < props.data.length; j += 1) {
+          const com = props.data[j];
           const next = Math.floor(
-            props.data[j].start.diff(beginning, 'months') /
-              total_duration *
-              WIDTH
+            com.start.diff(beginning, 'months') / total_duration * WIDTH
           );
           if (next >= end) {
-            console.log(end, next, `${job.company} & ${props.data[j].company}`);
+            console.log(end, next, `${job.company} & ${com.company}`);
+            // add fake in between end/start
+            // const gap = next - end;
+            // filler += (
+            //   {gap > 0 && <div style={{ ...styles.off, width: `${gap}%` }} />}
+            //   <div style={{ ...styles.on, width: `${mid}%` }}>
+            //     <div />
+            //     );
+            // pop off todo
           }
         }
         console.log('----------');
