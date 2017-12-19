2235afe9c4973d14bf928122e1de4637bf5a473c
kennethbigler <kennethbigler@gmail.com>
2017-12-19 08:44:26 -0800

    testing deployments

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,10 @@
 echo "Running build/deploy scripts..."
 npm run build
 echo -e "\n--------------------\nBuilding react-home \n--------------------"
-rm index.html
-cp -r build/index.html ./
-rm -rf ./static
-mv build/* ./
+# rm index.html
+# cp -r build/index.html ./
+# rm -rf ./static
+# mv build/* ./
 echo -e "Success \n--------------------\n"
 git status
 git add .
