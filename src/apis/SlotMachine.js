83eebef63ed2f52860a3b66592be0e476f51e6d2
kbigler <kbigler@cisco.com>
2018-01-08 16:43:30 -0800

    working on slots and mm

--- src/apis/SlotMachine.js
+++ src/apis/SlotMachine.js
@@ -9,8 +9,9 @@ export const DOUBLE_BAR = '=';
 export const TRIPLE_BAR = 'Ξ';
 export const SEVEN = '7';
 export const JACKPOT = 'J';
+export const NUMREELS = 3;
 
-const settings = [
+const reel = [
   { symbol: CHERRY, start: 1, stop: 2 },
   { symbol: EMPTY, start: 3, stop: 7 },
   { symbol: BAR, start: 8, stop: 12 },
@@ -37,16 +38,121 @@ const settings = [
 
 let machine = [];
 
-settings.forEach(slot => {
-  for (let i = slot.start; i <= slot.stop; i += 1) {
-    machine.push(slot.symbol);
+reel.forEach((slot, i) => {
+  // wrap the reel
+  const prev = i - 1 > 0 ? i - 1 : reel.length - 1;
+  const next = i + 1 < reel.length ? i + 1 : 0;
+  // create the machine
+  for (let j = slot.start; j <= slot.stop; j += 1) {
+    machine.push([reel[prev].symbol, slot.symbol, reel[next].symbol]);
   }
 });
 
+// spin the slot machine and get a result
+function spin() {
+  return machine[Math.floor(Math.random() * machine.length)];
+}
+
 export const SlotMachine = {
-  // spin the slot machine and get a result
-  spin: () => {
-    return machine[Math.floor(Math.random() * machine.length)];
+  // used to display the payout table
+  payoutTable: [
+    {
+      win: '3x',
+      symbol: JACKPOT,
+      payout: 1666
+    },
+    {
+      win: `${SEVEN} ${SEVEN}`,
+      symbol: SEVEN,
+      payout: 300
+    },
+    {
+      win: `${TRIPLE_BAR} ${TRIPLE_BAR}`,
+      symbol: TRIPLE_BAR,
+      payout: 100
+    },
+    {
+      win: `${DOUBLE_BAR} ${DOUBLE_BAR}`,
+      symbol: DOUBLE_BAR,
+      payout: 50
+    },
+    {
+      win: `${BAR} ${BAR}`,
+      symbol: BAR,
+      payout: 25
+    },
+    {
+      win: '3 of any',
+      symbol: 'bar',
+      payout: 12
+    },
+    {
+      win: '3x',
+      symbol: CHERRY,
+      payout: 12
+    },
+    {
+      win: '2x',
+      symbol: CHERRY,
+      payout: 6
+    },
+    {
+      win: '1x',
+      symbol: CHERRY,
+      payout: 3
+    }
+  ],
+  pullHandle: () => {
+    let reel = [];
+    for (let i = 0; i < NUMREELS; i += 1) {
+      reel[i] = spin()[1];
+    }
+    return reel;
+  },
+  // evaluate slot machine based of 3 reels
+  getPayout: (reel, bet) => {
+    // for bar check
+    const bars = [BAR, DOUBLE_BAR, TRIPLE_BAR];
+    // get reel values
+    const r0 = reel[0];
+    const r1 = reel[1];
+    const r2 = reel[2];
+    // check if they match
+    const m01 = r0 === r1;
+    const m02 = r0 === r2;
+    // if we have 3 of a kind
+    if (m01 && m02) {
+      switch (r0) {
+        case JACKPOT:
+          return 1666 * bet;
+        case SEVEN:
+          return 300 * bet;
+        case TRIPLE_BAR:
+          return 100 * bet;
+        case DOUBLE_BAR:
+          return 50 * bet;
+        case BAR:
+          return 25 * bet;
+        case CHERRY:
+          return 12 * bet;
+        default:
+          return 0;
+      }
+    } else if (
+      bars.indexOf(r0) !== -1 &&
+      bars.indexOf(r1) !== -1 &&
+      bars.indexOf(r2) !== -1
+    ) {
+      // if we have 3 of any bar
+      return 12 * bet;
+    } else if (reel.indexOf(CHERRY, reel.indexOf(CHERRY) + 1) !== -1) {
+      // if we have 2 cherries
+      return 6 * bet;
+    } else if (reel.indexOf(CHERRY) !== -1) {
+      // if we have 1 cherry
+      return 3 * bet;
+    }
+    return 0;
   }
 };
 
--- src/components/casino/Slots.js
+++ src/components/casino/Slots.js
@@ -3,15 +3,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { payout, updateBet } from '../../store/modules/players';
-import {
-  SlotMachine,
-  CHERRY,
-  BAR,
-  DOUBLE_BAR,
-  TRIPLE_BAR,
-  SEVEN,
-  JACKPOT
-} from '../../apis/SlotMachine';
+import { SlotMachine } from '../../apis/SlotMachine';
 import RaisedButton from 'material-ui/RaisedButton';
 import Paper from 'material-ui/Paper';
 import {
@@ -23,103 +15,6 @@ import {
   TableHeaderColumn
 } from 'material-ui/Table';
 // Parents: Main
-
-const NUMREELS = 3;
-
-function pullHandle() {
-  let reel = [];
-  for (let i = 0; i < NUMREELS; i += 1) {
-    reel[i] = SlotMachine.spin();
-  }
-  return reel;
-}
-
-function getPayout(reel, bet) {
-  const m01 = reel[0] === reel[1];
-  const m02 = reel[0] === reel[2];
-  const bars = [BAR, DOUBLE_BAR, TRIPLE_BAR];
-  // if we have 3 of a kind
-  if (m01 && m02) {
-    switch (reel[0]) {
-      case JACKPOT:
-        return 1666 * bet;
-      case SEVEN:
-        return 300 * bet;
-      case TRIPLE_BAR:
-        return 100 * bet;
-      case DOUBLE_BAR:
-        return 50 * bet;
-      case BAR:
-        return 25 * bet;
-      case CHERRY:
-        return 12 * bet;
-      default:
-        return 0;
-    }
-  } else if (
-    bars.indexOf(reel[0]) !== -1 &&
-    bars.indexOf(reel[1]) !== -1 &&
-    bars.indexOf(reel[2]) !== -1
-  ) {
-    // if we have 3 of any bar
-    return 12 * bet;
-  } else if (reel.indexOf(CHERRY, reel.indexOf(CHERRY) + 1) !== -1) {
-    // if we have 2 cherries
-    return 6 * bet;
-  } else if (reel.indexOf(CHERRY) !== -1) {
-    // if we have 1 cherry
-    return 3 * bet;
-  }
-  return 0;
-}
-
-const payoutTable = [
-  {
-    win: '3x',
-    symbol: JACKPOT,
-    payout: 1666
-  },
-  {
-    win: `${SEVEN} ${SEVEN}`,
-    symbol: SEVEN,
-    payout: 300
-  },
-  {
-    win: `${TRIPLE_BAR} ${TRIPLE_BAR}`,
-    symbol: TRIPLE_BAR,
-    payout: 100
-  },
-  {
-    win: `${DOUBLE_BAR} ${DOUBLE_BAR}`,
-    symbol: DOUBLE_BAR,
-    payout: 50
-  },
-  {
-    win: `${BAR} ${BAR}`,
-    symbol: BAR,
-    payout: 25
-  },
-  {
-    win: '3 of any',
-    symbol: 'bar',
-    payout: 12
-  },
-  {
-    win: '3x',
-    symbol: CHERRY,
-    payout: 12
-  },
-  {
-    win: '2x',
-    symbol: CHERRY,
-    payout: 6
-  },
-  {
-    win: '1x',
-    symbol: CHERRY,
-    payout: 3
-  }
-];
 
 /* --------------------------------------------------
 * Slot Machine
@@ -136,7 +31,7 @@ class SM extends Component {
     };
 
     this.state = {
-      reel: pullHandle()
+      reel: SlotMachine.pullHandle()
     };
   }
 
@@ -144,8 +39,8 @@ class SM extends Component {
     const { payout } = this.props.playerActions;
     const { id, money, bet } = this.props.players[0];
     const dealer = this.props.players[this.props.players.length - 1];
-    let reel = pullHandle();
-    let exchange = getPayout(reel, bet) - bet;
+    let reel = SlotMachine.pullHandle();
+    let exchange = SlotMachine.getPayout(reel, bet) - bet;
     payout(id, 'win', money, exchange);
     payout(dealer.id, 'win', dealer.money, -exchange);
     this.setState({ reel });
@@ -210,7 +105,7 @@ class SM extends Component {
                 </TableRow>
               </TableHeader>
               <TableBody displayRowCheckbox={false} showRowHover>
-                {payoutTable.map((row, i) => (
+                {SlotMachine.payoutTable.map((row, i) => (
                   <TableRow key={i}>
                     <TableRowColumn>
                       {row.win} {row.symbol}
