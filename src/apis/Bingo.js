070b2bae8ccdecda9621d5c59847b56aaf6f8894
kbigler <kbigler@cisco.com>
2018-02-05 15:24:10 -0800

    gi

--- src/apis/Bingo.js
+++ src/apis/Bingo.js
@@ -15,19 +15,3 @@ export const Bingo = {
     return ball;
   }
 };
-
-/*
-given an array of tasks (each number represents a different task)
-only 1 task can execute at a time, with a cool down in between the same tasks of the same number
-different tasks don't have to wait for the cooldown
-find how long execution of the tasks would take
-
-EX:
-nums: [1, 1, 2, 0, 2, 1]
-cooldown: 2
-Output: 9
-Explanation: 1 _ _ 1 2 0 _ 2 1
-*/
-
-// use a hash table, start at cooldown, subtract from there
-// inputs: array of tasks, cooldown
---
+++ src/apis/Interviews.js
@@ -0,0 +1,166 @@
+/* Facebook - Task Execution Time with Cooldowns
+given an array of tasks (each number represents a different task)
+only 1 task can execute at a time, with a cool down in between the same tasks of the same number
+different tasks don't have to wait for the cooldown
+find how long execution of the tasks would take
+
+EX:
+nums: [1, 1, 2, 0, 2, 1]
+cooldown: 2
+Output: 9
+Explanation: 1 _ _ 1 2 0 _ 2 1
+*/
+
+function doTasks(tasks, cooldown) {
+  let lastRunMap = {};
+  let ans = 0;
+  let debug = '';
+  let currentTime = 0;
+  for (let task of tasks) {
+    const lastRun = lastRunMap[task] || 0;
+    const waitTime = lastRun + cooldown + 1 - currentTime;
+    if (waitTime && lastRunMap[task] !== undefined) {
+      ans += waitTime;
+      for (let x = 0; x < waitTime; x++) {
+        debug += ' -';
+      }
+      currentTime += waitTime;
+    }
+    ans += 1;
+    debug += ` ${task}`;
+    lastRunMap[task] = currentTime;
+    currentTime += 1;
+  }
+  console.log(debug);
+  return ans;
+}
+
+doTasks([1, 1, 2, 0, 2, 1], 4);
+doTasks([1, 1, 2, 0, 2, 1], 4);
+doTasks([1, 1, 3, 2, 1, 2, 1], 3);
+
+/* Google - Is the input an Ambigram I
+Given an array of numbers, determine if it is an Ambigram
+Ambigram - The same word forwards, backwards, and upsidown
+EX: 1881
+
+HINT1: consider the numbers that flip to stay the same: 0, 1, 8
+HINT2: consider the numbers that flip to become another number: 6, 9
+*/
+function isAmbigram1(nums) {
+  // create map
+  const flip = { 0: 0, 1: 1, 6: 9, 9: 6, 8: 8 };
+  // verify characters are good
+  for (let i = 0; i < Math.ceil(nums.length / 2); i += 1) {
+    const a = nums[i];
+    const b = nums[nums.length - 1 - i];
+    // failed check
+    if (!flip[a] || flip[a] !== b) {
+      return false;
+    }
+  }
+  // everything passes check
+  return true;
+}
+
+isAmbigram1([1, 8, 1]);
+isAmbigram1([1, 6, 9, 1]);
+isAmbigram1([1, 6, 1]);
+
+/* Code Golf - Is the input an Ambigram II
+Given a string, determine if it is an Ambigram
+Ambigram - The same word forwards, backwards, and upsidown
+EX: 1881
+
+Table (goes both ways)
+0  0
+1  1
+2  5
+6  9
+8  8
+a  e
+b  q
+d  p/P
+h  y/Y
+l  l
+m  w
+n  u/U
+o  o/O
+O  o/O
+s  s/S
+t  t
+x  x/X
+z  z/Z
+H  H
+I  I
+M  W
+N  N
+*/
+function isAmbigram2(word) {
+  // create map
+  const flip = {
+    '0': ['0'],
+    '1': ['1'],
+    '2': ['5'],
+    '5': ['2'],
+    '6': ['9'],
+    '9': ['6'],
+    '8': ['8'],
+    a: ['e'],
+    e: ['a'],
+    b: ['q'],
+    q: ['b'],
+    d: ['p', 'P'],
+    p: ['d'],
+    P: ['d'],
+    h: ['y', 'Y'],
+    y: ['h'],
+    Y: ['h'],
+    l: ['l'],
+    m: ['w'],
+    w: ['m'],
+    n: ['u', 'U'],
+    u: ['n'],
+    U: ['n'],
+    o: ['o', 'O'],
+    O: ['o', 'O'],
+    s: ['s', 'S'],
+    S: ['s', 'S'],
+    t: ['t'],
+    x: ['x', 'X'],
+    X: ['x', 'X'],
+    z: ['z', 'Z'],
+    Z: ['z', 'Z'],
+    H: ['H'],
+    I: ['I'],
+    M: ['W'],
+    W: ['M'],
+    N: ['N']
+  };
+
+  const chars = word.split('');
+  // verify characters are good
+  for (let i = 0; i < Math.ceil(chars.length / 2); i += 1) {
+    // get 2 opposite elements
+    const arr = flip[chars[i]];
+    const b = chars[chars.length - 1 - i];
+    // check if failed check
+    if (!arr) return false;
+    // check for match, store in flag
+    let isMatch = false;
+    for (let j = 0; j < arr.length; j += 1) {
+      if (arr[j] === b) {
+        isMatch = true;
+        break;
+      }
+    }
+    // failed check
+    if (!isMatch) return false;
+  }
+  // everything passes check
+  return true;
+}
+
+isAmbigram2('181');
+isAmbigram2('1691');
+isAmbigram2('161');
