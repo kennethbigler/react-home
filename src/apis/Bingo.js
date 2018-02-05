b5b0d6bd3fa1c1cee1cf4fff991f11ca6b9a6df9
kennethbigler <kennethbigler@gmail.com>
2018-02-05 10:10:04 -0800

    fbi

--- src/apis/Bingo.js
+++ src/apis/Bingo.js
@@ -3,7 +3,7 @@
  * -------------------------------------------------- */
 
 let called = [true];
-const getRand = () =>  Math.floor(Math.random() * 75) + 1;
+const getRand = () => Math.floor(Math.random() * 75) + 1;
 
 export const Bingo = {
   getBall: () => {
@@ -13,5 +13,21 @@ export const Bingo = {
     }
     called[ball] = true;
     return ball;
-  },
+  }
 };
+
+/*
+given an array of tasks (each number represents a different task)
+only 1 task can execute at a time, with a cool down in between the same tasks of the same number
+different tasks don't have to wait for the cooldown
+find how long execution of the tasks would take
+
+EX:
+nums: [1, 1, 2, 0, 2, 1]
+cooldown: 2
+Output: 9
+Explanation: 1 _ _ 1 2 0 _ 2 1
+*/
+
+// use a hash table, start at cooldown, subtract from there
+// inputs: array of tasks, cooldown
