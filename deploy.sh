1ee55ee3ca2c12d5272a782ccafccd2d9127a95b
kennethbigler <kennethbigler@gmail.com>
2018-04-08 12:41:14 -0700

    finished work on deploy script now all build files are moved into a docs folder, hoping this should improve performance also got rid of image from http, moved to https

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 LINE="--------------------"
-echo $LINE
 echo "Running build scripts..."
 echo $LINE
 npm run build
 echo $LINE
 echo "Building react-home"
-echo $LINE
 rm -rf ./docs/static
 mv build/* ./docs
 rm -rf build
 echo $LINE
 echo "Success"
 echo $LINE
-# git status
-# git add .
-# git status
-# git commit
-# echo -e "\n--------------------\nComplete\n--------------------\nFinish with a:\ngit push\n"
+git status
+git add .
+git status
+git commit
+echo $LINE
+echo "Complete"
+echo $LINE
+echo -e "Finish with a:\ngit push\n"
