4a62385f1409f29145778709c1e43140ed8a033c
kennethbigler <kennethbigler@gmail.com>
2018-04-16 21:13:41 -0700

    expandable card improvements

--- src/components/common/ExpandableCard.js
+++ src/components/common/ExpandableCard.js
@@ -1,71 +1,83 @@
 // react
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 // material ui
 import { Card, CardTitle, CardText } from 'material-ui/Card';
 import * as colors from 'material-ui/styles/colors';
 
-export const ExpandableCard = props => {
-  const {
-    title,
-    subtitle,
-    children,
-    expandable,
-    backgroundColor,
-    ...otherProps
-  } = props;
-
-  const styles = {
-    open: {
-      backgroundColor: backgroundColor,
-      marginLeft: 15,
-      marginRight: 15,
-      top: -20,
-      borderRadius: 3,
-      boxShadow: `0px 15px 15px -10px ${colors.grey400}`
-    },
-    closed: {
-      backgroundColor: backgroundColor,
-      borderRadius: 3,
-      boxShadow: `0px 15px 15px -10px ${colors.grey400}`
-    }
+export class ExpandableCard extends Component {
+  static propTypes = {
+    // PropTypes = [string, object, bool, number, func, array].isRequired
+    backgroundColor: PropTypes.string,
+    title: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
+    subtitle: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
+    children: PropTypes.oneOfType([
+      PropTypes.arrayOf(PropTypes.node),
+      PropTypes.node
+    ])
   };
 
-  return (
-    <Card
-      {...otherProps}
-      style={{ marginTop: 40 }}
-      initiallyExpanded={expandable}
-    >
-      <CardTitle
-        title={title && title}
-        subtitle={subtitle && subtitle}
-        titleColor={colors.white}
-        subtitleColor={colors.grey400}
-        actAsExpander={expandable}
-        showExpandableButton={expandable}
-        style={styles.open}
-      />
-      <CardText expandable={expandable}>
-        <div className="row">{children}</div>
-      </CardText>
-    </Card>
-  );
-};
+  static defaultProps = {
+    backgroundColor: colors.blueGrey600
+  };
 
-ExpandableCard.propTypes = {
-  // PropTypes = [string, object, bool, number, func, array].isRequired
-  backgroundColor: PropTypes.string,
-  expandable: PropTypes.bool,
-  title: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
-  subtitle: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
-  children: PropTypes.oneOfType([
-    PropTypes.arrayOf(PropTypes.node),
-    PropTypes.node
-  ])
-};
+  state = {
+    expanded: true
+  };
 
-ExpandableCard.defaultProps = {
-  backgroundColor: colors.blueGrey600,
-  expandable: true
-};
+  handleExpandChange = expanded => {
+    this.setState({ expanded });
+  };
+
+  render() {
+    const {
+      title,
+      subtitle,
+      children,
+      backgroundColor,
+      ...otherProps
+    } = this.props;
+    const { expanded } = this.state;
+
+    let styles = {
+      card: { marginTop: 40 },
+      closedCard: { marginTop: 20 },
+      closed: {
+        backgroundColor: backgroundColor,
+        borderRadius: 3,
+        marginLeft: 10,
+        marginRight: 10,
+        boxShadow: `0px 15px 15px -10px ${colors.grey400}`
+      }
+    };
+    styles['open'] = {
+      ...styles.closed,
+      top: -20,
+      marginLeft: 15,
+      marginRight: 15
+    };
+
+    const style = expanded ? styles.open : styles.closed;
+
+    return (
+      <Card
+        expanded={expanded}
+        onExpandChange={this.handleExpandChange}
+        style={expanded ? styles.card : styles.closedCard}
+        {...otherProps}
+      >
+        <CardTitle
+          actAsExpander
+          style={style}
+          subtitle={subtitle}
+          subtitleColor={colors.grey400}
+          title={title}
+          titleColor={colors.white}
+        />
+        <CardText expandable>
+          <div className="row">{children}</div>
+        </CardText>
+      </Card>
+    );
+  }
+}
