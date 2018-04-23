fa97dbde62405648dc8842ad570982816e20f0c4
kennethbigler <kennethbigler@gmail.com>
2018-04-23 07:40:08 -0700

    keeping bootstrap

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
-import 'bootstrap/dist/css/bootstrap-grid.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
