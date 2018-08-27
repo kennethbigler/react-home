02b078d3285834ba2f543dabc7dbbbf0967854db
ken.w.bigler <Ken.W.Bigler@ey.com>
2018-08-27 16:08:10 -0700

    updating comments

--- src/apis/Interviews.js
+++ src/apis/Interviews.js
@@ -169,7 +169,7 @@ isAmbigram2('181');
 isAmbigram2('1691');
 isAmbigram2('161');
 
-/** GigNow - easy DP
+/** GigNow
  * Say you have an array for which the ith element is the price of a given stock on day i.
  *
  * If you were only permitted to complete at most one transaction
@@ -215,7 +215,7 @@ maxProfit([7, 1, 5, 3, 6, 4]) === 5;
 maxProfit([7, 6, 4, 3, 1]) === 0;
 maxProfit([7, 2, 8, 1, 5, 3, 6, 4]) === 6;
 
-/**
+/** GigNow
  * Find the intersection of two sorted arrays.
  * OR in other words,
  * Given 2 sorted arrays, find all the elements which occur in both the arrays.
