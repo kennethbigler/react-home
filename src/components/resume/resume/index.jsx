a1ebed0b73cbe40e0096abd0426e23e72570f27c
Wojciech Maj <kontakt@wojtekmaj.pl>
2018-10-05 22:17:43 +0200

    Use dynamic version to avoid future issues when pdfjs-dist gets updated

--- src/components/resume/resume/index.jsx
+++ src/components/resume/resume/index.jsx
@@ -5,7 +5,7 @@ import resume from '../../../images/kenneth_bigler_resume.pdf';
 // Parents: Main
 
 // Workaround for worker-loader failing on Webpack 4
-pdfjs.GlobalWorkerOptions.workerSrc = '//cdnjs.cloudflare.com/ajax/libs/pdf.js/2.0.550/pdf.worker.js';
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 export default class Resume extends Component {
   state = {
