af4b38f22dd1bc6e0788f272c78ad82e9f730396
kennethbigler <kennethbigler@gmail.com>
2018-10-31 21:26:11 -0700

    moving out of index

---
+++ src/components/games/blackjack/help/Help.jsx
@@ -0,0 +1,104 @@
+// react
+import React from 'react';
+// components
+// material-ui
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+// colors
+import green from '@material-ui/core/colors/green';
+import teal from '@material-ui/core/colors/teal';
+import red from '@material-ui/core/colors/red';
+import orange from '@material-ui/core/colors/orange';
+import deepPurple from '@material-ui/core/colors/deepPurple';
+import Cell from './Cell';
+import BlackjackTable from './BlackjackTable';
+// Parents: Popup
+
+const Help = () => {
+  // options
+  const h = { color: green[200], text: 'H' };
+  const d = { color: teal[200], text: 'D' };
+  const s = { color: red[200], text: 'S' };
+  const p = { color: orange[200], text: 'P' };
+  const ds = { color: deepPurple[200], text: 'DS' };
+
+  // algorithms for options
+  const hardTtl = [
+    { name: 'Hard 5', data: [h, h, h, h, h, h, h, h, h, h] },
+    { name: 'Hard 6', data: [h, h, h, h, h, h, h, h, h, h] },
+    { name: 'Hard 7', data: [h, h, h, h, h, h, h, h, h, h] },
+    { name: 'Hard 8', data: [h, h, h, h, h, h, h, h, h, h] },
+    { name: 'Hard 9', data: [h, d, d, d, d, h, h, h, h, h] },
+    { name: 'Hard 10', data: [d, d, d, d, d, d, d, d, h, h] },
+    { name: 'Hard 11', data: [d, d, d, d, d, d, d, d, d, d] },
+    { name: 'Hard 12', data: [h, h, s, s, s, h, h, h, h, h] },
+    { name: 'Hard 13', data: [s, s, s, s, s, h, h, h, h, h] },
+    { name: 'Hard 14', data: [s, s, s, s, s, h, h, h, h, h] },
+    { name: 'Hard 15', data: [s, s, s, s, s, h, h, h, h, h] },
+    { name: 'Hard 16', data: [s, s, s, s, s, h, h, h, h, h] },
+    { name: 'Hard 17', data: [s, s, s, s, s, s, s, s, s, s] },
+    { name: 'Hard 18+', data: [s, s, s, s, s, s, s, s, s, s] },
+  ];
+  const softTtl = [
+    { name: 'Ace + 2', data: [h, h, h, d, d, h, h, h, h, h] },
+    { name: 'Ace + 3', data: [h, h, h, d, d, h, h, h, h, h] },
+    { name: 'Ace + 4', data: [h, h, d, d, d, h, h, h, h, h] },
+    { name: 'Ace + 5', data: [h, h, d, d, d, h, h, h, h, h] },
+    { name: 'Ace + 6', data: [h, d, d, d, d, h, h, h, h, h] },
+    { name: 'Ace + 7', data: [ds, ds, ds, ds, ds, s, s, h, h, h] },
+    { name: 'Ace + 8', data: [s, s, s, s, ds, s, s, s, s, s] },
+    { name: 'Ace + 9', data: [s, s, s, s, s, s, s, s, s, s] },
+  ];
+  const pairs = [
+    { name: '(2,2)', data: [p, p, p, p, p, p, h, h, h, h] },
+    { name: '(3,3)', data: [p, p, p, p, p, p, h, h, h, h] },
+    { name: '(4,4)', data: [h, h, h, p, p, h, h, h, h, h] },
+    { name: '(5,5)', data: [d, d, d, d, d, d, d, d, h, h] },
+    { name: '(6,6)', data: [p, p, p, p, p, h, h, h, h, h] },
+    { name: '(7,7)', data: [p, p, p, p, p, p, h, h, h, h] },
+    { name: '(8,8)', data: [p, p, p, p, p, p, p, p, p, p] },
+    { name: '(9,9)', data: [p, p, p, p, p, s, p, p, s, s] },
+    { name: '(T,T)', data: [s, s, s, s, s, s, s, s, s, s] },
+    { name: '(A,A)', data: [p, p, p, p, p, p, p, p, p, p] },
+  ];
+
+  return (
+    <div>
+      <BlackjackTable data={hardTtl} title="Hard Totals" />
+      <BlackjackTable data={softTtl} title="Soft Totals" />
+      <BlackjackTable data={pairs} title="Pairs" />
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell colSpan="11">
+              Key
+            </TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          <TableRow>
+            <Cell {...h} />
+            <Cell colSpan="2" text="= Hit" />
+            <Cell {...s} />
+            <Cell colSpan="3" text="= Stand" />
+            <Cell {...p} />
+            <Cell colSpan="3" text="= Split" />
+          </TableRow>
+          <TableRow>
+            <Cell {...d} />
+            <Cell colSpan="10" text="= Double (Hit if not allowed)" />
+          </TableRow>
+          <TableRow>
+            <Cell {...ds} />
+            <Cell colSpan="10" text="= Double (Stand if not allowed)" />
+          </TableRow>
+        </TableBody>
+      </Table>
+    </div>
+  );
+};
+
+export default Help;
---
+++ src/components/games/slots/Slots.jsx
@@ -0,0 +1,188 @@
+// react
+import React, { Component } from 'react';
+import types from 'prop-types';
+// redux
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+// components
+// material ui
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+// functions
+import map from 'lodash/map';
+import DarkTableCell from '../../common/DarkTableCell';
+import SlotMachine from './SlotMachine';
+import { payout, updateBet } from '../../../store/modules/players';
+// Parents: Main
+
+/* --------------------------------------------------
+* Slot Machine
+* -------------------------------------------------- */
+class Slots extends Component {
+  // Prop Validation
+  static propTypes = {
+    // types = [array, bool, func, number, object, string, symbol].isRequired
+    playerActions: types.shape({
+      payout: types.func.isRequired,
+      updateBet: types.func.isRequired,
+    }).isRequired,
+    players: types.arrayOf(
+      types.shape({
+        id: types.number.isRequired,
+        money: types.number.isRequired,
+        bet: types.number.isRequired,
+      }),
+    ).isRequired,
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      reel: SlotMachine.pullHandle(),
+    };
+  }
+
+  updateSlotMachine = () => {
+    const { playerActions, players } = this.props;
+    const { id, bet } = players[0];
+    const dealer = players[players.length - 1];
+    const reel = SlotMachine.pullHandle();
+    const exchange = SlotMachine.getPayout(reel, bet) - bet;
+    playerActions.payout(id, 'win', exchange);
+    playerActions.payout(dealer.id, 'win', -exchange);
+    this.setState({ reel });
+  };
+
+  // https://vegasclick.com/games/slots/how-they-work
+  render() {
+    const { reel } = this.state;
+    const { players } = this.props;
+    const player = players[0];
+    const dealer = players[players.length - 1];
+
+    // generate code for slot machine
+    const slots = [];
+    for (let i = 0; i < 3; i += 1) {
+      // create 3 cells in a row
+      const row = [];
+      for (let j = 0; j < 3; j += 1) {
+        const slotCell = (
+          <TableCell key={`${j},${i}`}>
+            {reel[j][i]}
+          </TableCell>
+        );
+        row.push(slotCell);
+      }
+      // separate into rows
+      const slotRow = (
+        <TableRow key={`row${i}`}>
+          {row}
+        </TableRow>
+      );
+      slots.push(slotRow);
+    }
+
+    return (
+      <div>
+        <h1>
+          Welcome to Ken&apos;s Casino Slot Machine
+        </h1>
+        <Grid container spacing={16} style={{ marginTop: '2em' }}>
+          <Grid item sm={6} xs={12}>
+            <Grid container spacing={16} style={{ marginBottom: '1em' }}>
+              <Grid item sm={3} xs={12}>
+                <Button
+                  color="primary"
+                  onClick={this.updateSlotMachine}
+                  style={{ marginBottom: 15 }}
+                  variant="contained"
+                >
+                  Spin
+                </Button>
+              </Grid>
+              <Grid item sm={9} xs={12}>
+                <Table>
+                  <TableBody>
+                    {slots}
+                  </TableBody>
+                </Table>
+              </Grid>
+            </Grid>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <DarkTableCell>
+                    Player
+                  </DarkTableCell>
+                  <DarkTableCell>
+                    Money
+                  </DarkTableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                <TableRow>
+                  <TableCell>
+                    {player.name}
+                  </TableCell>
+                  <TableCell>
+                    {`$${player.money}`}
+                  </TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>
+                    House
+                  </TableCell>
+                  <TableCell>
+                    {`$${dealer.money}`}
+                  </TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </Grid>
+          <Grid item sm={6} xs={12}>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <DarkTableCell>
+                    Slot Roll
+                  </DarkTableCell>
+                  <DarkTableCell>
+                    Payout
+                  </DarkTableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {map(SlotMachine.payoutTable, (row, i) => (
+                  <TableRow key={i}>
+                    <TableCell>
+                      {`${row.win} ${row.symbol}`}
+                    </TableCell>
+                    <TableCell>
+                      {`${row.payout} : 1`}
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </Grid>
+        </Grid>
+      </div>
+    );
+  }
+}
+
+// react-redux export
+const mapStateToProps = state => ({ players: state.players });
+const mapDispatchToProps = dispatch => ({
+  playerActions: bindActionCreators({ payout, updateBet }, dispatch),
+});
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Slots);
