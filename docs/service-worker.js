56d9361fa4c071bf14891d44e3021701c1ade67a
kennethbigler <kennethbigler@gmail.com>
2019-02-17 09:17:50 -0800

    updated resume

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a8d21d368d7c577899508c9292d4d99c.js"
+  "/precache-manifest.be2ed01f28018471599ed8c30b89ccd0.js"
 );
 
 workbox.clientsClaim();
