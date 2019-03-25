988cc17ec7f5b84623882bf25110c2d0e0641899
Ken <kennethbigler@gmail.com>
2019-03-25 15:30:15 -0700

    deploying new poker scores

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.924871d3306c50b5d8f9097a723495bf.js"
+  "/precache-manifest.c02c372c7882c0cc6a6059ef300c1de7.js"
 );
 
 workbox.clientsClaim();
