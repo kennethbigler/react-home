6ea2a28fb37f4e4784b29a4052e54f80ff035c87
kennethbigler <kennethbigler@gmail.com>
2019-03-25 18:22:18 -0700

    upgrade packages and resume

---
+++ docs/precache-manifest.7363a43f863d9d3ff69bda87ff323ff7.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "98fb09649d961ae444739392f6768e8d",
+    "url": "/static/media/tesla_motors_logo.svg.98fb0964.png"
+  },
+  {
+    "revision": "7082636547ed6dfb4aec",
+    "url": "/static/css/main.2d37cc28.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "227cb61749df2e5d1168",
+    "url": "/static/js/2.7caea8fa.chunk.js"
+  },
+  {
+    "revision": "8957e4395b6e60df72005b18622bbbe9",
+    "url": "/static/media/ken.8957e439.jpg"
+  },
+  {
+    "revision": "fa723ab1130824dbc0b941569297c0d3",
+    "url": "/static/media/gignow.fa723ab1.png"
+  },
+  {
+    "revision": "50fa2212b0c3365c1e1987a642e1dfe0",
+    "url": "/static/media/SHFB_logo.50fa2212.jpg"
+  },
+  {
+    "revision": "e90724402bf5e92ed80b577abf550236",
+    "url": "/static/media/netapp_logo.svg.e9072440.png"
+  },
+  {
+    "revision": "4f8455945889eca9f0fd97d33e4a8160",
+    "url": "/static/media/vengefulgames_logo.4f845594.png"
+  },
+  {
+    "revision": "7082636547ed6dfb4aec",
+    "url": "/static/js/main.a0bbc05b.chunk.js"
+  },
+  {
+    "revision": "3477c7e96c98b8d2c521bbf42cafbde7",
+    "url": "/static/media/hoverboard_logo.3477c7e9.png"
+  },
+  {
+    "revision": "2f758223d58aa094a70dbd4c427dcf36",
+    "url": "/static/media/kenneth_bigler_resume.2f758223.pdf"
+  },
+  {
+    "revision": "873d13e716eafb9c7fe3efbd23a0702f",
+    "url": "/static/media/07_toyota_prius.873d13e7.jpg"
+  },
+  {
+    "revision": "61f4d2d8c93944aeb4134f7cc95228f6",
+    "url": "/static/media/93_pontiac_bonneville.61f4d2d8.jpg"
+  },
+  {
+    "revision": "0784f67d019d5bfe312d5d5029182a46",
+    "url": "/static/media/10_chevrolet_impala_ls.0784f67d.jpg"
+  },
+  {
+    "revision": "9d4061ac010bba86dda2ed410bd02c6b",
+    "url": "/static/media/15_mustang_gt_premium.9d4061ac.jpg"
+  },
+  {
+    "revision": "a67ee1ec820da2cf58ff681bc77e67e0",
+    "url": "/static/media/05_jaguar_xj8l.a67ee1ec.jpg"
+  },
+  {
+    "revision": "819ef16b525abae82c7e325b52f79ed7",
+    "url": "/index.html"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c02c372c7882c0cc6a6059ef300c1de7.js"
+  "/precache-manifest.7363a43f863d9d3ff69bda87ff323ff7.js"
 );
 
 workbox.clientsClaim();
