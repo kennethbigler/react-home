6859e68ea8f4d236b26d884a5722eee5ea1df3ca
Ken <Kenneth_Bigler@intuit.com>
2019-10-25 08:11:29 -0700

    work on Resume to ts

--- src/components/resume/resume/Resume.tsx
+++ src/components/resume/resume/Resume.tsx
@@ -1,24 +1,29 @@
 import React, { useState, memo } from 'react';
-// https://github.com/wojtekmaj/react-pdf
 import { pdfjs, Document, Page } from 'react-pdf';
 import get from 'lodash/get';
 import resume from '../../../images/kenneth_bigler_resume.pdf';
-// Parents: Main
 
 const MAX_SCALE = 1.5;
 
 // Workaround for worker-loader failing on Webpack 4
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
-function useResume() {
-  const [numPages, setNumPages] = useState(null);
+interface ResumeHook {
+  numPages: number;
+  width: number;
+  onDocumentLoadSuccess: Function;
+  onLoadSuccess: Function;
+}
+
+function useResume(): ResumeHook {
+  const [numPages, setNumPages] = useState(0);
   const [width, setWidth] = useState(0);
 
-  const onDocumentLoadSuccess = (pdf) => {
+  const onDocumentLoadSuccess = (pdf): void => {
     setNumPages(get(pdf, 'numPages', 1));
   };
 
-  const onLoadSuccess = (pdf) => {
+  const onLoadSuccess = (pdf): void => {
     const pdfWidth = get(pdf, 'pageInfo.view[2]', 612);
     const screenWidth = document.body.clientWidth - 32;
     setWidth(screenWidth > pdfWidth * MAX_SCALE ? pdfWidth * MAX_SCALE : screenWidth);
