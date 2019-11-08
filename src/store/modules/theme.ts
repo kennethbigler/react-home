51aaa2810dcaa4eabe45451194bb09c8176f4b8b
Ken <kennethbigler@gmail.com>
2019-11-07 17:43:34 -0800

    new dark theme color and update packages

--- src/store/modules/theme.ts
+++ src/store/modules/theme.ts
@@ -1,7 +1,7 @@
 import { Action } from 'redux';
 import blueGrey from '@material-ui/core/colors/blueGrey';
 import deepOrange from '@material-ui/core/colors/deepOrange';
-import indigo from '@material-ui/core/colors/indigo';
+import blue from '@material-ui/core/colors/blue';
 
 import { DBUITheme } from '../types';
 import initialState from '../initialState';
@@ -18,7 +18,7 @@ export const displayLightTheme = (): Action => ({ type: LIGHT_THEME });
 export default function reducer(state: DBUITheme = initialState.theme, action: Action): DBUITheme {
   switch (action.type) {
     case DARK_THEME:
-      return { ...state, ...{ type: 'dark', primary: indigo, secondary: deepOrange }};
+      return { ...state, ...{ type: 'dark', primary: blue, secondary: deepOrange }};
     case LIGHT_THEME:
       return { ...state, ...{ type: 'light', primary: blueGrey, secondary: deepOrange }};
     default:
