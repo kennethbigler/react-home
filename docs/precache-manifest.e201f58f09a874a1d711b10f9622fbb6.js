28ad0703c09b7606cdbb59652debadff5e19db06
kbigler <Ken_Bigler@intuit.com>
2019-11-08 13:17:06 -0800

    deploying state reduction and dateObj fix

---
+++ docs/precache-manifest.e201f58f09a874a1d711b10f9622fbb6.js
@@ -0,0 +1,210 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62a05d475801505aa28b4c28516aae7d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b300bbc00bbfad848045",
+    "url": "/static/css/main.f5b75174.chunk.css"
+  },
+  {
+    "revision": "fc8a7e179af73e8dfc9f",
+    "url": "/static/js/0.ea453897.chunk.js"
+  },
+  {
+    "revision": "dc6f5453471ec85455d4",
+    "url": "/static/js/1.94c1328e.chunk.js"
+  },
+  {
+    "revision": "1a1c0d7dbdd6194de912",
+    "url": "/static/js/2.225a6b20.chunk.js"
+  },
+  {
+    "revision": "9f4a0c162d815117bfb5",
+    "url": "/static/js/3.5842ecb0.chunk.js"
+  },
+  {
+    "revision": "6dd805bbb9cdd1860ee7",
+    "url": "/static/js/31.de0bc24d.chunk.js"
+  },
+  {
+    "revision": "69f0e9d5c7dae20a57b7",
+    "url": "/static/js/32.6d7fb460.chunk.js"
+  },
+  {
+    "revision": "165cd2e693ac3e748f9b",
+    "url": "/static/js/33.6925772d.chunk.js"
+  },
+  {
+    "revision": "c3be60ded5d6507be128",
+    "url": "/static/js/34.28a31fe3.chunk.js"
+  },
+  {
+    "revision": "47ceaa199d6eb1954db2",
+    "url": "/static/js/35.a2e96c7d.chunk.js"
+  },
+  {
+    "revision": "d070c19ce09bb8c1378a",
+    "url": "/static/js/4.d648a8d0.chunk.js"
+  },
+  {
+    "revision": "2a860a6929dc0d911049",
+    "url": "/static/js/5.d5a001bb.chunk.js"
+  },
+  {
+    "revision": "7198cb5da05e17420b11",
+    "url": "/static/js/6.6e4741ab.chunk.js"
+  },
+  {
+    "revision": "77db883d18d7f5217706",
+    "url": "/static/js/7.e3bbe236.chunk.js"
+  },
+  {
+    "revision": "1c83d59380595b4682e4",
+    "url": "/static/js/g_bj.50566cb7.chunk.js"
+  },
+  {
+    "revision": "f8e93e06b6128e3b5f4b",
+    "url": "/static/js/g_connect4.3e0f6727.chunk.js"
+  },
+  {
+    "revision": "100fbd4e511d58f7a57a",
+    "url": "/static/js/g_d2p.4e671c1e.chunk.js"
+  },
+  {
+    "revision": "d4df18a7a83569dffaa2",
+    "url": "/static/js/g_dond.1b9b3ca7.chunk.js"
+  },
+  {
+    "revision": "1461d09613adb561b21a",
+    "url": "/static/js/g_home.e7cd0008.chunk.js"
+  },
+  {
+    "revision": "c7fe913adc71546cb5b9",
+    "url": "/static/js/g_poker.6255d9f7.chunk.js"
+  },
+  {
+    "revision": "3d192f81a59b115a7368",
+    "url": "/static/js/g_slots.0e1bc9b1.chunk.js"
+  },
+  {
+    "revision": "66ffc1afa2e12f61fb7f",
+    "url": "/static/js/g_tictactoe.aa055751.chunk.js"
+  },
+  {
+    "revision": "c223c4740f087189da57",
+    "url": "/static/js/g_yahtzee.727b136f.chunk.js"
+  },
+  {
+    "revision": "0319494d6437a80c8428",
+    "url": "/static/js/games.328dbf55.chunk.js"
+  },
+  {
+    "revision": "b300bbc00bbfad848045",
+    "url": "/static/js/main.43d07ad4.chunk.js"
+  },
+  {
+    "revision": "4df9b48575f5f44e7a3f",
+    "url": "/static/js/r_cars.86a34011.chunk.js"
+  },
+  {
+    "revision": "7a991c45a45d14e9b003",
+    "url": "/static/js/r_education.08056a51.chunk.js"
+  },
+  {
+    "revision": "2210aef773a3c8aaf77f",
+    "url": "/static/js/r_git.4aacd37e.chunk.js"
+  },
+  {
+    "revision": "a092a456c6e85fee8e30",
+    "url": "/static/js/r_graphql.e963ff46.chunk.js"
+  },
+  {
+    "revision": "0239995e0cce30ffb626",
+    "url": "/static/js/r_poker.6066ee7a.chunk.js"
+  },
+  {
+    "revision": "43828033560ec3ffa317",
+    "url": "/static/js/r_resume.f496fd17.chunk.js"
+  },
+  {
+    "revision": "f9ed9d8074e29de4f24c",
+    "url": "/static/js/r_summary.9ecd2b0b.chunk.js"
+  },
+  {
+    "revision": "6b2ff6b8b06e1e26187a",
+    "url": "/static/js/r_travel.a8e9ff05.chunk.js"
+  },
+  {
+    "revision": "75889a4d4f6fb19c4e50",
+    "url": "/static/js/r_work.6dbf4a32.chunk.js"
+  },
+  {
+    "revision": "de5b0b8d58025c17369d",
+    "url": "/static/js/r_work_bar_chart.6c343025.chunk.js"
+  },
+  {
+    "revision": "f57d2c1d18287dae8242",
+    "url": "/static/js/resume.35fd3c95.chunk.js"
+  },
+  {
+    "revision": "4e4a933ad48e2531a080",
+    "url": "/static/js/runtime-main.91ff9e9f.js"
+  },
+  {
+    "revision": "a67ee1ec820da2cf58ff681bc77e67e0",
+    "url": "/static/media/05_jaguar_xj8l.a67ee1ec.jpg"
+  },
+  {
+    "revision": "873d13e716eafb9c7fe3efbd23a0702f",
+    "url": "/static/media/07_toyota_prius.873d13e7.jpg"
+  },
+  {
+    "revision": "0784f67d019d5bfe312d5d5029182a46",
+    "url": "/static/media/10_chevrolet_impala_ls.0784f67d.jpg"
+  },
+  {
+    "revision": "9d4061ac010bba86dda2ed410bd02c6b",
+    "url": "/static/media/15_mustang_gt_premium.9d4061ac.jpg"
+  },
+  {
+    "revision": "61f4d2d8c93944aeb4134f7cc95228f6",
+    "url": "/static/media/93_pontiac_bonneville.61f4d2d8.jpg"
+  },
+  {
+    "revision": "50fa2212b0c3365c1e1987a642e1dfe0",
+    "url": "/static/media/SHFB_logo.50fa2212.jpg"
+  },
+  {
+    "revision": "fa723ab1130824dbc0b941569297c0d3",
+    "url": "/static/media/gignow_logo.fa723ab1.png"
+  },
+  {
+    "revision": "3477c7e96c98b8d2c521bbf42cafbde7",
+    "url": "/static/media/hoverboard_logo.3477c7e9.png"
+  },
+  {
+    "revision": "03e890f5b2f235db495a65355a89a398",
+    "url": "/static/media/intuit_logo.03e890f5.png"
+  },
+  {
+    "revision": "7f70b8a2d87ffa04a1ffde828be0819a",
+    "url": "/static/media/ken.7f70b8a2.jpg"
+  },
+  {
+    "revision": "ecd3383cd04391aa9afcb43b7e129fd9",
+    "url": "/static/media/kenneth_bigler_resume.ecd3383c.png"
+  },
+  {
+    "revision": "e90724402bf5e92ed80b577abf550236",
+    "url": "/static/media/netapp_logo.svg.e9072440.png"
+  },
+  {
+    "revision": "98fb09649d961ae444739392f6768e8d",
+    "url": "/static/media/tesla_motors_logo.svg.98fb0964.png"
+  },
+  {
+    "revision": "4f8455945889eca9f0fd97d33e4a8160",
+    "url": "/static/media/vengefulgames_logo.4f845594.png"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1d9efd7cbb217413a606c2867624c513.js"
+  "/precache-manifest.e201f58f09a874a1d711b10f9622fbb6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
