e2ae239f0957d426c2fd566ec196e694013fc025
kbigler <Ken_Bigler@intuit.com>
2019-11-15 09:20:56 -0800

    remove scoregraph state

--- src/components/games/yahtzee/ScoreGraph.tsx
+++ src/components/games/yahtzee/ScoreGraph.tsx
@@ -12,66 +12,42 @@ interface ScoreGraphProps {
   theme: Theme;
 }
 interface ScoreGraphEntry {
-  // TODO: See if we can remove score from here
-  score: number;
   value: number;
 }
-interface ScoreGraphState {
-  scores: ScoreGraphEntry[];
-}
 
-const generateScoreGraph = (scores: number[]): ScoreGraphState => ({
-  scores: scores.map((score) => ({ score, value: score })),
-});
+const generateScoreGraph = (scores: number[]): ScoreGraphEntry[] => (
+  scores.map((value) => ({ value }))
+);
 
-class ScoreGraph extends React.Component<ScoreGraphProps, ScoreGraphState> {
-  constructor(props: ScoreGraphProps) {
-    super(props);
-    this.state = generateScoreGraph(props.scores);
-  }
+const ScoreGraph: React.FC<ScoreGraphProps> = (props: ScoreGraphProps) => {
+  const { scores: dBScores, theme: { palette: { secondary: { main }}}} = props;
 
-  static getDerivedStateFromProps: React.GetDerivedStateFromProps<ScoreGraphProps, ScoreGraphState> = (props, state) => {
-    if (props.scores.length > state.scores.length) {
-      return generateScoreGraph(props.scores);
-    }
-    return null;
-  }
+  const scores = React.useMemo(() => generateScoreGraph(dBScores), [dBScores]);
+  const recent = scores[scores.length - 1] || { value: 0 };
+  const mostRecent = recent.value;
+  const top = maxBy(scores, 'value') || { value: 0 };
+  const topScore = top.value;
 
-  shouldComponentUpdate(nextProps: ScoreGraphProps): boolean {
-    const { scores } = this.props;
-    return nextProps.scores.length > scores.length;
-  }
-
-  render(): React.ReactNode {
-    const { theme: { palette: { secondary: { main }}}} = this.props;
-    const { scores } = this.state;
-
-    const recent = scores[scores.length - 1] || { score: 0 };
-    const mostRecent = recent.score;
-    const top = maxBy(scores, 'score') || { score: 0 };
-    const topScore = top.score;
-
-    return (
-      <InfoPopup title="Yahtzee Score History" buttonText="Score History">
-        <ResponsiveContainer width="100%" height={300}>
-          <AreaChart data={scores}>
-            <defs>
-              <linearGradient id="mainColor" x1="0" y1="0" x2="0" y2="1">
-                <stop offset="20%" stopColor={main} stopOpacity={0.8} />
-                <stop offset="100%" stopColor={main} stopOpacity={0} />
-              </linearGradient>
-            </defs>
-            <XAxis dataKey="name" interval="preserveStartEnd" />
-            <YAxis width={40} orientation="right" tickLine={false} />
-            <Tooltip />
-            <Area type="monotone" dataKey="score" stroke={main} fillOpacity={1} fill="url(#mainColor)" />
-          </AreaChart>
-        </ResponsiveContainer>
-        <Typography variant="h4">{`Most Recent: ${mostRecent}`}</Typography>
-        <Typography variant="h4">{`Top Score: ${topScore}`}</Typography>
-      </InfoPopup>
-    );
-  }
-}
+  return (
+    <InfoPopup title="Yahtzee Score History" buttonText="Score History">
+      <ResponsiveContainer width="100%" height={300}>
+        <AreaChart data={scores}>
+          <defs>
+            <linearGradient id="mainColor" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="20%" stopColor={main} stopOpacity={0.8} />
+              <stop offset="100%" stopColor={main} stopOpacity={0} />
+            </linearGradient>
+          </defs>
+          <XAxis dataKey="name" interval="preserveStartEnd" />
+          <YAxis width={40} orientation="right" tickLine={false} />
+          <Tooltip />
+          <Area type="monotone" dataKey="value" stroke={main} fillOpacity={1} fill="url(#mainColor)" />
+        </AreaChart>
+      </ResponsiveContainer>
+      <Typography variant="h4">{`Most Recent: ${mostRecent}`}</Typography>
+      <Typography variant="h4">{`Top Score: ${topScore}`}</Typography>
+    </InfoPopup>
+  );
+};
 
 export default withTheme(ScoreGraph);
