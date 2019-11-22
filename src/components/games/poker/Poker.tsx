cd85639a72da01a4a541b41ddde305853d0ce26c
kbigler <Ken_Bigler@intuit.com>
2019-11-22 09:46:00 -0800

    async improved, hooks added, still some async issues

--- src/components/games/poker/Poker.tsx
+++ src/components/games/poker/Poker.tsx
@@ -42,37 +42,105 @@ interface PokerDBState extends DBPoker {
 }
 interface PokerProps extends PokerDBState, PokerActions {}
 
-const Poker: React.FC<PokerProps> = (props: PokerProps) => {
+interface UsePokerFunctions {
+  checkUpdate: () => Promise<void>;
+  handleGameFunctionClick: (type: PGF) => void;
+}
+
+const usePokerFunctions = (props: PokerProps): UsePokerFunctions => {
   const {
-    turn: { player: turn }, players, cardsToDiscard, gameFunctions,
-    gameOver, hideHands,
+    players, cardsToDiscard, gameOver, hideHands,
+    turn: { player: turn },
   } = props;
 
-  const newGame = (): void => {
-    const { pokerActions } = props;
-    pokerActions.newPokerGame(players);
-  };
-
-  /** function to finish betting and start the game */
-  const startGame = (): void => {
-    const { pokerActions, playerActions } = props;
-
-    pokerActions.startPokerGame();
-    // shuffle the deck
-    Deck.shuffle().then(() => {
-      // deal the hands
-      asyncForEach(players, async (player: DBPlayer) => {
-        if (player.id !== DEALER && player.id <= LAST_PLAYER) {
-          await playerActions.newHand(player.id, 5);
-        }
-      });
-    });
-  };
-
+  // ----------     bot automation handlers     ---------- //
   /** increment player turn and reset state */
-  const endTurn = (): void => {
+  const endTurn = async (): Promise<void> => {
     const { pokerActions } = props;
-    pokerActions.endPokerTurn();
+    await pokerActions.endPokerTurn();
+  };
+
+  /** iterate through array, removing each index number from hand
+   * then add new cards to the hand
+   */
+  const discard = async (cardsToDiscardInDB: number[]): Promise<void> => {
+    const { playerActions } = props;
+
+    const { id, hands } = players[turn];
+    await playerActions.swapCards(hands, id, cardsToDiscardInDB);
+  };
+
+  /** function to remove n number of cards */
+  const discardHelper = async (n: number, hist: number[]): Promise<void> => {
+    const hand = getHand(players, turn);
+    const nextCardsToDiscard = [];
+    const cardValues = [hist.indexOf(1)];
+    // find cards without pairs, starting with the smallest
+    for (let i = 1; i < n; i += 1) {
+      cardValues[i] = hist.indexOf(1, cardValues[i - 1] + 1);
+    }
+    // find hand index of individual cards
+    for (let i = 0; i < hand.length; i += 1) {
+      for (let j = 0; j < cardValues.length; j += 1) {
+        if (hand[i].weight - 2 === cardValues[j]) {
+          nextCardsToDiscard.push(i);
+          break;
+        }
+      }
+    }
+
+    // discard lowest, non-pair n cards
+    await discard(nextCardsToDiscard);
+  };
+
+  /** computer play algorithm:
+   * PAIRS
+   * draw 0 on 4 of a kind
+   * draw 0 on full house
+   * draw 1 on 3 of a kind, keep higher of 2
+   * draw 1 on 2 pair
+   * draw 3 on 2 of a kind
+   *
+   * This is a nice to have, for now we only follow the first half
+   * STRAIGHT/FLUSH
+   * draw 0 on straight
+   * draw 0 on flush
+   * draw 0 on straight flush
+   * if 1 away from sf -> draw 1
+   * if 1 away from S -> draw 1 if 5+ players, else regular hand
+   * if 1 away from F -> draw 1 if 5+ players, else regular hand
+   *
+   * REGULAR HAND
+   * if K / A -> draw 4
+   * else draw 5
+   */
+  const computer = async (): Promise<void> => {
+    const hand = getHand(players, turn);
+    const hist = getHistogram(hand);
+    const rank = rankHand(hand, hist);
+
+    switch (rank) {
+      case 0: // draw 4-5 on high card
+        hist.lastIndexOf(1) >= 11
+          ? await discardHelper(4, hist) // if ace || king draw 4
+          : await discard([0, 1, 2, 3, 4]); // otherwise, draw all 5
+        break;
+      case 1: // draw 3 on 2 of a kind
+        await discardHelper(3, hist);
+        break;
+      case 2: // draw 1 on 3 of a kind
+      case 3: // draw 1 on 2 Pair
+        await discardHelper(1, hist);
+        break;
+      case 4: // draw 0 on straight
+      case 5: // draw 0 on flush
+      case 6: // draw 0 on full house
+      case 7: // draw 0 on 4 of a kind
+      case 8: // draw 0 on straight flush
+      default:
+        break;
+    }
+    await endTurn();
   };
 
   const endGame = (): void => {
@@ -99,14 +167,36 @@ const Poker: React.FC<PokerProps> = (props: PokerProps) => {
     pokerActions.endPokerGame();
   };
 
-  /** iterate through array, removing each index number from hand
-   * then add new cards to the hand
-   */
-  const discard = (cardsToDiscardInDB: number[]): void => {
-    const { playerActions } = props;
+  const checkUpdate = async (): Promise<void> => {
+    const player: DBPlayer = players[turn];
 
-    const { id, hands } = players[turn];
-    playerActions.swapCards(hands, id, cardsToDiscardInDB);
+    if (!hideHands && !gameOver && player.isBot) {
+      const canPlay: boolean = player.id !== DEALER && player.id <= LAST_PLAYER;
+      console.log(canPlay ? `computer(${player.id})` : 'endGame()', player);
+      canPlay ? await computer() : endGame();
+    }
+  };
+
+  // ----------     player handlers     ---------- //
+  const newGame = (): void => {
+    const { pokerActions } = props;
+    pokerActions.newPokerGame(players);
+  };
+
+  /** function to finish betting and start the game */
+  const startGame = (): void => {
+    const { pokerActions, playerActions } = props;
+
+    pokerActions.startPokerGame();
+    // shuffle the deck
+    Deck.shuffle().then(() => {
+      // deal the hands
+      asyncForEach(players, async (player: DBPlayer) => {
+        if (player.id !== DEALER && player.id <= LAST_PLAYER) {
+          await playerActions.newHand(player.id, 5);
+        }
+      });
+    });
   };
 
   /** helper function wrapping discard, meant for UI */
@@ -114,91 +204,6 @@ const Poker: React.FC<PokerProps> = (props: PokerProps) => {
     const { pokerActions } = props;
     discard(cardsToDiscard);
     pokerActions.discardCards();
-  };
-
-  /** function to remove n number of cards */
-  const discardHelper = (n: number, hist: number[]): void => {
-    const hand = getHand(players, turn);
-    const nextCardsToDiscard = [];
-    const cardValues = [hist.indexOf(1)];
-    // find cards without pairs, starting with the smallest
-    for (let i = 1; i < n; i += 1) {
-      cardValues[i] = hist.indexOf(1, cardValues[i - 1] + 1);
-    }
-    // find hand index of individual cards
-    for (let i = 0; i < hand.length; i += 1) {
-      for (let j = 0; j < cardValues.length; j += 1) {
-        if (hand[i].weight - 2 === cardValues[j]) {
-          nextCardsToDiscard.push(i);
-          break;
-        }
-      }
-    }
-
-    // discard lowest, non-pair n cards
-    discard(nextCardsToDiscard);
-  };
-
-  /** computer play algorithm:
-   * PAIRS
-   * draw 0 on 4 of a kind
-   * draw 0 on full house
-   * draw 1 on 3 of a kind, keep higher of 2
-   * draw 1 on 2 pair
-   * draw 3 on 2 of a kind
-   *
-   * This is a nice to have, for now we only follow the first half
-   * STRAIGHT/FLUSH
-   * draw 0 on straight
-   * draw 0 on flush
-   * draw 0 on straight flush
-   * if 1 away from sf -> draw 1
-   * if 1 away from S -> draw 1 if 5+ players, else regular hand
-   * if 1 away from F -> draw 1 if 5+ players, else regular hand
-   *
-   * REGULAR HAND
-   * if K / A -> draw 4
-   * else draw 5
-   */
-  const computer = (): void => {
-    const hand = getHand(players, turn);
-    const hist = getHistogram(hand);
-    const rank = rankHand(hand, hist);
-
-    switch (rank) {
-      case 0: // draw 4-5 on high card
-        hist.lastIndexOf(1) >= 11
-          ? discardHelper(4, hist) // if ace || king draw 4
-          : discard([0, 1, 2, 3, 4]); // otherwise, draw all 5
-        break;
-      case 1: // draw 3 on 2 of a kind
-        discardHelper(3, hist);
-        break;
-      case 2: // draw 1 on 3 of a kind
-      case 3: // draw 1 on 2 Pair
-        discardHelper(1, hist);
-        break;
-      case 4: // draw 0 on straight
-      case 5: // draw 0 on flush
-      case 6: // draw 0 on full house
-      case 7: // draw 0 on 4 of a kind
-      case 8: // draw 0 on straight flush
-      default:
-        break;
-    }
-    endTurn();
-  };
-
-  /** function to be called on card clicks */
-  const cardClickHandler = (playerNo: number, handNo: number, cardNo: number): void => {
-    const { pokerActions } = props;
-    const newCardsToDiscard = [...cardsToDiscard];
-    // find card
-    const i = newCardsToDiscard.indexOf(cardNo);
-    // toggle in array
-    i === -1 ? newCardsToDiscard.push(cardNo) : newCardsToDiscard.splice(i, 1);
-    // update state
-    pokerActions.updateCardsToDiscard(newCardsToDiscard);
   };
 
   /** function to route click actions */
@@ -218,19 +223,33 @@ const Poker: React.FC<PokerProps> = (props: PokerProps) => {
     }
   };
 
-  const checkUpdate = (): void => {
-    const player: DBPlayer = players[turn];
+  return {
+    checkUpdate,
+    handleGameFunctionClick,
+  };
+};
 
-    if (!hideHands && !gameOver && player.isBot) {
-      const canPlay: boolean = player.id !== DEALER && player.id <= LAST_PLAYER;
-      canPlay ? computer() : endGame();
-      console.log(canPlay ? 'computer()' : 'endGame()');
-    }
+const Poker: React.FC<PokerProps> = (props: PokerProps) => {
+  const {
+    turn, players, cardsToDiscard, gameFunctions,
+    gameOver, hideHands,
+  } = props;
+
+  /** function to be called on card clicks */
+  const cardClickHandler = (playerNo: number, handNo: number, cardNo: number): void => {
+    const { pokerActions } = props;
+    const newCardsToDiscard = [...cardsToDiscard];
+    // find card
+    const i = newCardsToDiscard.indexOf(cardNo);
+    // toggle in array
+    i === -1 ? newCardsToDiscard.push(cardNo) : newCardsToDiscard.splice(i, 1);
+    // update state
+    pokerActions.updateCardsToDiscard(newCardsToDiscard);
   };
 
-  checkUpdate();
+  const { checkUpdate, handleGameFunctionClick } = usePokerFunctions(props);
 
-  const playerTurn = React.useMemo(() => ({ player: turn, hand: 0 }), [turn]);
+  checkUpdate();
 
   return (
     <>
@@ -246,7 +265,7 @@ const Poker: React.FC<PokerProps> = (props: PokerProps) => {
         hideHands={hideHands}
         isBlackJack={false}
         players={players}
-        turn={playerTurn}
+        turn={turn}
       />
     </>
   );
