f9351bc4c0645969cb25fe87b9a5aee0a476bc58
Ken Bigler <kennethbigler@gmail.com>
2020-05-23 15:33:08 -0700

    test: dice

---
+++ src/apis/Dice.test.ts
@@ -0,0 +1,28 @@
+import Dice, { diceRoller, useDice } from './Dice';
+
+describe('apis | Dice', () => {
+  it('rolls a number between 1 and 6', () => {
+    for (let i = 0; i < 10; i += 1) {
+      const roll = diceRoller();
+      expect(roll).toBeLessThanOrEqual(6);
+      expect(roll).toBeGreaterThanOrEqual(1);
+    }
+  });
+
+  it('useDice rolls a number between 1 and 6', () => {
+    const diceRoller2 = useDice();
+    for (let i = 0; i < 10; i += 1) {
+      const roll = diceRoller2();
+      expect(roll).toBeLessThanOrEqual(6);
+      expect(roll).toBeGreaterThanOrEqual(1);
+    }
+  });
+
+  it('default export rolls a number between 1 and 6', () => {
+    for (let i = 0; i < 10; i += 1) {
+      const roll = Dice.roll();
+      expect(roll).toBeLessThanOrEqual(6);
+      expect(roll).toBeGreaterThanOrEqual(1);
+    }
+  });
+});
--- src/apis/Dice.ts
+++ src/apis/Dice.ts
@@ -1,16 +1,18 @@
 import { Dice } from '../store/types';
 
+export const diceRoller = (): Dice => Math.floor(Math.random() * 6) + 1 as Dice;
+
 export default {
   // randomize order of the cards
   roll(): Dice {
-    return Math.floor(Math.random() * 6) + 1 as Dice;
+    return diceRoller();
   },
 };
 
 // Dice as a react hook, seems kind of like an anti-pattern
 export function useDice(): () => number {
   function rollDice(): number {
-    return Math.floor(Math.random() * 6) + 1;
+    return diceRoller();
   }
 
   return rollDice;
