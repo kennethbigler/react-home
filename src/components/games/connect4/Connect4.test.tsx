d2150e6e00ff360cde3bdc3254ff2b8a2af950fc
Ken Bigler <kennethbigler@gmail.com>
2021-03-14 08:50:00 -0700

    fix: connect4 times and link coverage

--- src/components/games/connect4/Connect4.test.tsx
+++ src/components/games/connect4/Connect4.test.tsx
@@ -9,35 +9,41 @@ describe('games | connect4 | Connect4', () => {
 
     expect(screen.getByText("Welcome to Ken's Connect4 Game")).toBeInTheDocument();
     expect(screen.getByText('Turn:')).toBeInTheDocument();
+    // get buttons only once to save time
+    const buttons = screen.getAllByRole('button');
     // button 0, should be red because they goes first
-    expect(screen.getAllByRole('button')[0]).toHaveStyle({ backgroundColor: 'rgb(244, 67, 54)' });
+    expect(buttons[0]).toHaveStyle({ backgroundColor: 'rgb(244, 67, 54)' });
     // button 1
     expect(screen.getByText('Reset Game')).toBeInTheDocument();
     // button 2-8 are to play the game
-    expect(screen.getAllByRole('button')[2]).toHaveStyle({ backgroundColor: 'rgb(244, 67, 54)' });
-    expect(screen.getAllByRole('button')[8]).toHaveStyle({ backgroundColor: 'rgb(244, 67, 54)' });
+    expect(buttons[2]).toHaveStyle({ backgroundColor: 'rgb(244, 67, 54)' });
+    expect(buttons[8]).toHaveStyle({ backgroundColor: 'rgb(244, 67, 54)' });
     // 9-51 are for display
-    expect(screen.getAllByRole('button')).toHaveLength(51);
+    expect(buttons).toHaveLength(51);
   });
+
   it('plays a piece and can reset after', () => {
     render(<Connect4 />);
 
-    expect(screen.getAllByRole('button')[0]).toHaveStyle({ backgroundColor: 'rgb(244, 67, 54)' });
-    expect(screen.getAllByRole('button')[2]).toHaveStyle({ backgroundColor: 'rgb(244, 67, 54)' });
-    expect(screen.getAllByRole('button')[8]).toHaveStyle({ backgroundColor: 'rgb(244, 67, 54)' });
-    expect(screen.getAllByRole('button')[44]).not.toHaveStyle({ backgroundColor: 'rgb(244, 67, 54)' });
+    let buttons = screen.getAllByRole('button');
+    expect(buttons[0]).toHaveStyle({ backgroundColor: 'rgb(244, 67, 54)' });
+    expect(buttons[2]).toHaveStyle({ backgroundColor: 'rgb(244, 67, 54)' });
+    expect(buttons[8]).toHaveStyle({ backgroundColor: 'rgb(244, 67, 54)' });
+    expect(buttons[44]).not.toHaveStyle({ backgroundColor: 'rgb(244, 67, 54)' });
 
-    fireEvent.click(screen.getAllByRole('button')[2]);
-    expect(screen.getAllByRole('button')[0]).toHaveStyle({ backgroundColor: 'black' });
-    expect(screen.getAllByRole('button')[2]).toHaveStyle({ backgroundColor: 'black' });
-    expect(screen.getAllByRole('button')[8]).toHaveStyle({ backgroundColor: 'black' });
-    expect(screen.getAllByRole('button')[44]).toHaveStyle({ backgroundColor: 'rgb(244, 67, 54)' });
+    fireEvent.click(buttons[2]);
+    buttons = screen.getAllByRole('button');
+    expect(buttons[0]).toHaveStyle({ backgroundColor: 'black' });
+    expect(buttons[2]).toHaveStyle({ backgroundColor: 'black' });
+    expect(buttons[8]).toHaveStyle({ backgroundColor: 'black' });
+    expect(buttons[44]).toHaveStyle({ backgroundColor: 'rgb(244, 67, 54)' });
 
     fireEvent.click(screen.getByText('Reset Game'));
-    expect(screen.getAllByRole('button')[0]).toHaveStyle({ backgroundColor: 'rgb(244, 67, 54)' });
-    expect(screen.getAllByRole('button')[2]).toHaveStyle({ backgroundColor: 'rgb(244, 67, 54)' });
-    expect(screen.getAllByRole('button')[8]).toHaveStyle({ backgroundColor: 'rgb(244, 67, 54)' });
-    expect(screen.getAllByRole('button')[44]).not.toHaveStyle({ backgroundColor: 'rgb(244, 67, 54)' });
+    buttons = screen.getAllByRole('button');
+    expect(buttons[0]).toHaveStyle({ backgroundColor: 'rgb(244, 67, 54)' });
+    expect(buttons[2]).toHaveStyle({ backgroundColor: 'rgb(244, 67, 54)' });
+    expect(buttons[8]).toHaveStyle({ backgroundColor: 'rgb(244, 67, 54)' });
+    expect(buttons[44]).not.toHaveStyle({ backgroundColor: 'rgb(244, 67, 54)' });
   });
 
   it('marks the winner', () => {
@@ -48,19 +54,23 @@ describe('games | connect4 | Connect4', () => {
 
     fireEvent.click(screen.getByText('Reset Game'));
 
-    fireEvent.click(screen.getAllByRole('button')[2]);
-    fireEvent.click(screen.getAllByRole('button')[3]);
-    fireEvent.click(screen.getAllByRole('button')[2]);
-    fireEvent.click(screen.getAllByRole('button')[3]);
-    fireEvent.click(screen.getAllByRole('button')[2]);
-    fireEvent.click(screen.getAllByRole('button')[3]);
-    expect(screen.getAllByRole('button')[44]).toHaveStyle('background-color: rgb(244, 67, 54)');
-    fireEvent.click(screen.getAllByRole('button')[2]);
+    let buttons = screen.getAllByRole('button');
+    fireEvent.click(buttons[2]);
+    fireEvent.click(buttons[3]);
+    fireEvent.click(buttons[2]);
+    fireEvent.click(buttons[3]);
+    fireEvent.click(buttons[2]);
+    fireEvent.click(buttons[3]);
+    buttons = screen.getAllByRole('button');
+    expect(buttons[44]).toHaveStyle('background-color: rgb(244, 67, 54)');
+    fireEvent.click(buttons[2]);
 
     expect(screen.getByText('Winner:')).toBeInTheDocument();
-    expect(screen.getAllByRole('button')[0]).toHaveStyle({ backgroundColor: 'rgb(244, 67, 54)' });
-    expect(screen.getAllByRole('button')[44]).toHaveStyle('background-color: rgb(124, 179, 66);');
+    buttons = screen.getAllByRole('button');
+    expect(buttons[0]).toHaveStyle({ backgroundColor: 'rgb(244, 67, 54)' });
+    expect(buttons[44]).toHaveStyle('background-color: rgb(124, 179, 66);');
   });
+
   it('recognizes 5 in a row', () => {
     render(<Connect4 />);
 
@@ -69,19 +79,22 @@ describe('games | connect4 | Connect4', () => {
 
     fireEvent.click(screen.getByText('Reset Game'));
 
-    fireEvent.click(screen.getAllByRole('button')[2]);
-    fireEvent.click(screen.getAllByRole('button')[2]);
-    fireEvent.click(screen.getAllByRole('button')[3]);
-    fireEvent.click(screen.getAllByRole('button')[3]);
-    fireEvent.click(screen.getAllByRole('button')[4]);
-    fireEvent.click(screen.getAllByRole('button')[4]);
-    fireEvent.click(screen.getAllByRole('button')[6]);
-    fireEvent.click(screen.getAllByRole('button')[6]);
-    expect(screen.getAllByRole('button')[44]).toHaveStyle('background-color: rgb(244, 67, 54)');
-    fireEvent.click(screen.getAllByRole('button')[5]);
+    let buttons = screen.getAllByRole('button');
+    fireEvent.click(buttons[2]);
+    fireEvent.click(buttons[2]);
+    fireEvent.click(buttons[3]);
+    fireEvent.click(buttons[3]);
+    fireEvent.click(buttons[4]);
+    fireEvent.click(buttons[4]);
+    fireEvent.click(buttons[6]);
+    fireEvent.click(buttons[6]);
+    buttons = screen.getAllByRole('button');
+    expect(buttons[44]).toHaveStyle('background-color: rgb(244, 67, 54)');
+    fireEvent.click(buttons[5]);
 
     expect(screen.getByText('Winner:')).toBeInTheDocument();
-    expect(screen.getAllByRole('button')[0]).toHaveStyle({ backgroundColor: 'rgb(244, 67, 54)' });
-    expect(screen.getAllByRole('button')[44]).toHaveStyle('background-color: rgb(124, 179, 66);');
+    buttons = screen.getAllByRole('button');
+    expect(buttons[0]).toHaveStyle({ backgroundColor: 'rgb(244, 67, 54)' });
+    expect(buttons[44]).toHaveStyle('background-color: rgb(124, 179, 66);');
   });
 });
---
+++ src/components/resume/graphql/repository/item/tests/Link.test.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Link from '../Link';
+
+describe('resume | graphql | Link', () => {
+  it('renders as expected', () => {
+    render(<Link href="www.kennethbigler.com">Text</Link>);
+
+    expect(screen.getByText('Text')).toBeInTheDocument();
+    expect(screen.getByText('Text')).toHaveAttribute('href', 'www.kennethbigler.com');
+  });
+});
