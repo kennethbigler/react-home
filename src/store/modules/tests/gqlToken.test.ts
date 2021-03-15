fadd4cf773708108e147337b5bc8d10770330bec
Ken Bigler <kennethbigler@gmail.com>
2021-03-14 19:32:04 -0700

    test: gql reducer

---
+++ src/store/modules/tests/gqlToken.test.ts
@@ -0,0 +1,12 @@
+import gqlReducer, { setToken } from '../gqlToken';
+
+describe('store | modules | gql', () => {
+  test('reducer', () => {
+    expect(gqlReducer('', setToken('hello world'))).toEqual('hello world');
+    expect(gqlReducer('hello world', setToken())).toEqual('');
+    // @ts-expect-error: fake action for testing purposes
+    expect(gqlReducer('test', { type: undefined })).toEqual('test');
+    // @ts-expect-error: fake action for testing purposes
+    expect(gqlReducer(undefined, { type: undefined })).toEqual('');
+  });
+});
