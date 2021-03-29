5ff6a787c5aaefd48c05932d3d30441db74130ab
Ken Bigler <kennethbigler@gmail.com>
2021-03-29 15:20:04 -0700

    test: dnd

--- src/components/games/deal-or-no-deal/tests/DealOrNoDeal.test.tsx
+++ src/components/games/deal-or-no-deal/tests/DealOrNoDeal.test.tsx
@@ -14,8 +14,8 @@ describe('games | deal-or-no-deal | DealOrNoDeal', () => {
     expect(screen.getAllByRole('button')).toHaveLength(26);
   });
 
-  it('selects a case', () => {
-    const { debug } = render(<DealOrNoDeal />);
+  it('plays a game and takes final deal', async () => {
+    render(<DealOrNoDeal />);
 
     expect(screen.getByText('Your Case: ?')).toBeInTheDocument();
     expect(screen.getByText('Number of Cases to Open: 6')).toBeInTheDocument();
@@ -28,6 +28,70 @@ describe('games | deal-or-no-deal | DealOrNoDeal', () => {
     expect(screen.getByText('Your Case: 7')).toBeInTheDocument();
     expect(screen.getByText('Number of Cases to Open: 6')).toBeInTheDocument();
     expect(screen.getByText('Ken: $0')).toBeInTheDocument();
+    expect(screen.getAllByRole('button')).toHaveLength(26);
+
+    // open cases 1-6
+    fireEvent.click(screen.getAllByRole('button')[0]);
+    fireEvent.click(screen.getAllByRole('button')[1]);
+    fireEvent.click(screen.getAllByRole('button')[2]);
+    fireEvent.click(screen.getAllByRole('button')[3]);
+    fireEvent.click(screen.getAllByRole('button')[4]);
+    fireEvent.click(screen.getAllByRole('button')[5]);
+    // this one should not open
+    fireEvent.click(screen.getAllByRole('button')[7]);
+
+    await new Promise((r) => setTimeout(r, 301));
+
+    expect(screen.getAllByText('$1')).toBeDefined();
+    expect(screen.getAllByText('$10')).toBeDefined();
+    expect(screen.getAllByText('$100')).toBeDefined();
+    expect(screen.getAllByText('$1,000')).toBeDefined();
+    expect(screen.getAllByText('$10,000')).toBeDefined();
+    expect(screen.getAllByText('$100,000')).toBeDefined();
+    expect(screen.getAllByText('$1,000,000')).toBeDefined();
+    expect(screen.getByText('Deal')).toBeInTheDocument();
+    expect(screen.getByText('No Deal')).toBeInTheDocument();
+    // there should only be 2 buttons now
+    expect(screen.getAllByRole('button')).toHaveLength(2);
+
+    fireEvent.click(screen.getByText('No Deal'));
+
+    await new Promise((r) => setTimeout(r, 201));
+
+    expect(screen.getAllByRole('button')).toHaveLength(26);
+
+    // open cases 8-12
+    fireEvent.click(screen.getAllByRole('button')[7]);
+    fireEvent.click(screen.getAllByRole('button')[8]);
+    fireEvent.click(screen.getAllByRole('button')[9]);
+    fireEvent.click(screen.getAllByRole('button')[10]);
+    fireEvent.click(screen.getAllByRole('button')[11]);
+
+    await new Promise((r) => setTimeout(r, 301));
+
+    expect(screen.getAllByText('$1')).toBeDefined();
+    expect(screen.getAllByText('$10')).toBeDefined();
+    expect(screen.getAllByText('$100')).toBeDefined();
+    expect(screen.getAllByText('$1,000')).toBeDefined();
+    expect(screen.getAllByText('$10,000')).toBeDefined();
+    expect(screen.getAllByText('$100,000')).toBeDefined();
+    expect(screen.getAllByText('$1,000,000')).toBeDefined();
+    expect(screen.getByText('Deal')).toBeInTheDocument();
+    expect(screen.getByText('No Deal')).toBeInTheDocument();
+    // there should only be 2 buttons now
+    expect(screen.getAllByRole('button')).toHaveLength(2);
+
+    fireEvent.click(screen.getByText('Deal'));
+
+    await new Promise((r) => setTimeout(r, 201));
+
+    expect(screen.getByText('New Game')).toBeInTheDocument();
+    expect(screen.getAllByRole('button')).toHaveLength(27);
+
+    fireEvent.click(screen.getByText('New Game'));
+
+    expect(screen.getByText('Your Case: ?')).toBeInTheDocument();
+    expect(screen.getByText('Number of Cases to Open: 6')).toBeInTheDocument();
     expect(screen.getAllByRole('button')).toHaveLength(26);
   });
 });
