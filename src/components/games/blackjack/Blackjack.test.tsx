bb5e3a1effe3a35edcad0fdc8db5a281b56bcb74
Ken Bigler <kennethbigler@gmail.com>
2021-03-31 16:25:06 -0700

    test: blackjack

--- src/components/games/blackjack/Blackjack.test.tsx
+++ src/components/games/blackjack/Blackjack.test.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
-import { screen, fireEvent } from '@testing-library/react';
+import { screen, fireEvent, waitFor } from '@testing-library/react';
 import render from '../../../redux-test-render';
+import Deck from '../../../apis/Deck';
 import Blackjack from './Blackjack';
+
+const deck = jest.spyOn(Deck, 'deal');
+
+const ace = { name: 'A', weight: 14, suit: '♣' };
+const king = { name: 'K', weight: 13, suit: '♣' };
+const queen = { name: 'Q', weight: 12, suit: '♣' };
+const jack = { name: 'J', weight: 11, suit: '♣' };
+const nine = { name: '9', weight: 9, suit: '♣' };
 
 describe('games | blackjack | Blackjack', () => {
   it('renders as expected', () => {
@@ -27,24 +36,109 @@ describe('games | blackjack | Blackjack', () => {
     expect(screen.getByText('Close')).toBeInTheDocument();
   });
 
-  // TODO: this test does not finish betting, something async may be happening?
-  it.skip('plays a game', () => {
-    jest.useFakeTimers();
-    const { debug } = render(<Blackjack />);
-    jest.runAllTimers();
+  it("can't HIT on a blackjack", async () => {
+    deck.mockReset()
+      .mockResolvedValueOnce([king, ace])
+      .mockResolvedValueOnce([king, queen])
+      .mockResolvedValueOnce([king, queen])
+      .mockResolvedValueOnce([king, queen])
+      .mockResolvedValueOnce([king, queen])
+      .mockResolvedValueOnce([king, queen])
+      .mockResolvedValueOnce([king])
+      .mockResolvedValue([jack]);
 
-    expect(screen.getByText('Blackjack (21)')).toBeInTheDocument();
-    expect(screen.getByText('Rules')).toBeInTheDocument();
-    expect(screen.getByText('Ken: $100')).toBeInTheDocument();
-    expect(screen.getAllByText('Bet: $5')).toHaveLength(6);
-    expect(screen.getAllByText('Bot: $100')).toHaveLength(5);
-    expect(screen.getByText('Dealer: $100')).toBeInTheDocument();
-    expect(screen.getByText('Finish Betting')).toBeInTheDocument();
+    render(<Blackjack />);
 
     fireEvent.click(screen.getByText('Finish Betting'));
-    jest.runAllTimers();
+    await waitFor(() => screen.getByText('Hand: 21'));
+    expect(screen.getByText('Stay')).toBeInTheDocument();
+    expect(screen.queryByText('Hit')).toBeNull();
+    expect(screen.queryByText('Double')).toBeNull();
+    expect(screen.queryByText('Split')).toBeNull();
+    expect(screen.getAllByText('Hand: 20')).toHaveLength(5);
+    expect(screen.getByText('Hand: 10')).toBeInTheDocument();
+    fireEvent.click(screen.getByText('Stay'));
+    await waitFor(() => screen.getByText('New Game'));
+    fireEvent.click(screen.getByText('New Game'));
+    fireEvent.click(screen.getByText('Finish Betting'));
+  });
 
-    debug();
-    expect(true);
+  it('can HIT on a hand, stay, then start a new game', async () => {
+    deck.mockReset()
+      .mockResolvedValueOnce([king, queen])
+      .mockResolvedValueOnce([king, queen])
+      .mockResolvedValueOnce([king, queen])
+      .mockResolvedValueOnce([king, queen])
+      .mockResolvedValueOnce([king, queen])
+      .mockResolvedValueOnce([king, queen])
+      .mockResolvedValueOnce([king])
+      .mockResolvedValue([jack]);
+
+    render(<Blackjack />);
+
+    fireEvent.click(screen.getByText('Finish Betting'));
+    await waitFor(() => screen.getAllByText('Hand: 20'));
+    expect(screen.getAllByText('Hand: 20')).toHaveLength(6);
+    expect(screen.getByText('Hand: 10')).toBeInTheDocument();
+    fireEvent.click(screen.getByText('Hit'));
+    await waitFor(() => screen.getByText('Bust: 30'));
+    fireEvent.click(screen.getByText('Stay'));
+    await waitFor(() => screen.getByText('New Game'));
+    fireEvent.click(screen.getByText('New Game'));
+    fireEvent.click(screen.getByText('Finish Betting'));
+  });
+
+  it('can DOUBLE on a hand, then start a new game', async () => {
+    deck.mockReset()
+      .mockResolvedValueOnce([king, queen])
+      .mockResolvedValueOnce([king, queen])
+      .mockResolvedValueOnce([king, queen])
+      .mockResolvedValueOnce([king, queen])
+      .mockResolvedValueOnce([king, queen])
+      .mockResolvedValueOnce([king, queen])
+      .mockResolvedValueOnce([king])
+      .mockResolvedValue([jack]);
+
+    render(<Blackjack />);
+
+    fireEvent.click(screen.getByText('Finish Betting'));
+    await waitFor(() => screen.getAllByText('Hand: 20'));
+    expect(screen.getAllByText('Hand: 20')).toHaveLength(6);
+    expect(screen.getByText('Hand: 10')).toBeInTheDocument();
+    expect(screen.getAllByText('Bet: $5')).toHaveLength(6);
+    fireEvent.click(screen.getByText('Double'));
+    await waitFor(() => screen.getByText('Bust: 30'));
+    expect(screen.getAllByText('Bet: $5')).toHaveLength(5);
+    expect(screen.getByText('Bet: $10')).toBeInTheDocument();
+    await waitFor(() => screen.getByText('New Game'));
+    fireEvent.click(screen.getByText('New Game'));
+    fireEvent.click(screen.getByText('Finish Betting'));
+  });
+
+  it('can SPLIT on a hand, then start a new game', async () => {
+    deck.mockReset()
+      .mockResolvedValueOnce([king, queen])
+      .mockResolvedValueOnce([king, queen])
+      .mockResolvedValueOnce([king, queen])
+      .mockResolvedValueOnce([king, queen])
+      .mockResolvedValueOnce([king, queen])
+      .mockResolvedValueOnce([king, queen])
+      .mockResolvedValueOnce([king])
+      .mockResolvedValue([nine]);
+
+    render(<Blackjack />);
+
+    fireEvent.click(screen.getByText('Finish Betting'));
+    await waitFor(() => screen.getAllByText('Hand: 20'));
+    expect(screen.getAllByText('Hand: 20')).toHaveLength(6);
+    expect(screen.getByText('Hand: 10')).toBeInTheDocument();
+    fireEvent.click(screen.getByText('Split'));
+    await waitFor(() => screen.getAllByText('Hand: 19'));
+    expect(screen.getAllByText('Hand: 19')).toHaveLength(2);
+    fireEvent.click(screen.getByText('Stay'));
+    fireEvent.click(screen.getByText('Stay'));
+    await waitFor(() => screen.getByText('New Game'));
+    fireEvent.click(screen.getByText('New Game'));
+    fireEvent.click(screen.getByText('Finish Betting'));
   });
 });
