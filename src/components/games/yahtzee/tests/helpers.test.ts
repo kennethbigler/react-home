04844c0d53543dd3b9e9055172866885fb915d4d
Ken Bigler <kennethbigler@gmail.com>
2021-04-21 14:50:50 -0700

    fix: make scoreTableHelper coverage consistent

--- src/components/games/yahtzee/tests/helpers.test.ts
+++ src/components/games/yahtzee/tests/helpers.test.ts
@@ -1,4 +1,7 @@
 import getYahtzeeVars from '../helpers';
+import {
+  hasXDice, isFullHouse, isStraight, canYahtzeeBonus,
+} from '../score-table/scoreTableHelper';
 
 describe('games | yahtzee | helpers', () => {
   it('gets Yahtzee vars', () => {
@@ -8,5 +11,25 @@ describe('games | yahtzee | helpers', () => {
     expect(getYahtzeeVars([60, 1, 1, 1, 1, 1, 1, 1, 1, 1], [1, 1, 1, 1])).toEqual({
       topSum: 69, bottomSum: 4, finalTopSum: 104, finish: true,
     });
+  });
+
+  test('hasXDice', () => {
+    expect(hasXDice([1, 1, 2, 2, 2], 3)).toStrictEqual(true);
+    expect(hasXDice([1, 1, 2, 2, 3], 3)).toStrictEqual(false);
+  });
+
+  test('isFullHouse', () => {
+    expect(isFullHouse([1, 1, 2, 2, 2])).toStrictEqual(true);
+    expect(isFullHouse([1, 1, 2, 2, 3])).toStrictEqual(false);
+  });
+
+  test('isStraight', () => {
+    expect(isStraight([1, 2, 3, 4, 5], 5)).toStrictEqual(true);
+    expect(isStraight([1, 1, 2, 2, 3], 4)).toStrictEqual(false);
+  });
+
+  test('canYahtzeeBonus', () => {
+    expect(canYahtzeeBonus([1, 1, 1, 1, 1], [{ name: 'Ken', score: 5 }])).toStrictEqual(true);
+    expect(canYahtzeeBonus([1, 1, 2, 2, 3], [{ name: 'Ken', score: 5 }])).toStrictEqual(false);
   });
 });
