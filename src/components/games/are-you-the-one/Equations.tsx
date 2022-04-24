e2b572fedf9b350696c9c6682be403b14ca25110
Ken Bigler <kennethbigler@gmail.com>
2022-04-24 09:51:17 -0700

    feat: adding in odds

--- src/components/games/are-you-the-one/Equations.tsx
+++ src/components/games/are-you-the-one/Equations.tsx
@@ -9,19 +9,43 @@ interface EquationsProps {
   gents: string[];
 }
 
+interface AYTOHist {
+  value: number;
+  maxOdds: number;
+}
+
 const Equations = (props: EquationsProps) => {
   const { ladies, gents } = props;
   // Redux
   const { roundPairings, noMatch, matches } = useSelector(
     (state: DBRootState) => ({ ...state.ayto })
   );
-  const hist: number[][] = [];
+  const hist: AYTOHist[][] = [];
+  const tempScore: number[] = [];
+  const totals: number[] = [];
 
   // create histogram
-  roundPairings.forEach((RP) => {
-    RP.pairs.forEach((gi, li) => {
+  roundPairings.forEach(({ score, pairs }, ri) => {
+    tempScore[ri] = score;
+    totals[ri] = ladies.length;
+    pairs.forEach((gi, li) => {
+      if (li < 0 || gi < 0 || noMatch[li][gi]) {
+        totals[ri] -= 1;
+      }
+      if (matches[li] === gi) {
+        tempScore[ri] -= 1;
+        totals[ri] -= 1;
+      }
       !hist[li] && (hist[li] = []);
-      hist[li][gi] = !hist[li][gi] ? 1 : hist[li][gi] + 1;
+      !hist[li][gi] && (hist[li][gi] = { value: 0, maxOdds: 0 });
+      hist[li][gi].value += 1;
+    });
+  });
+
+  roundPairings.forEach(({ pairs }, ri) => {
+    pairs.forEach((gi, li) => {
+      const odds = Math.floor((tempScore[ri] * 100) / totals[ri]);
+      hist[li][gi].maxOdds = Math.max(hist[li][gi].maxOdds, odds);
     });
   });
 
@@ -29,24 +53,19 @@ const Equations = (props: EquationsProps) => {
     <div>
       <h1>Equations</h1>
       <Stack spacing={2} direction="row">
-        {roundPairings.map(({ pairs, score }, ri) => {
-          let tempScore = score;
+        {roundPairings.map(({ pairs }, ri) => {
           const equation = pairs.map((gi, li) => {
-            // if noMatch, skip
-            if (noMatch[li][gi] || li < 0 || gi < 0) {
+            // if noMatch or match, skip
+            if (li < 0 || gi < 0 || noMatch[li][gi] || matches[li] === gi) {
               return null;
             }
-            if (matches[li] === gi) {
-              tempScore -= 1;
-              return null;
-            }
-            const isRepeat = hist[li][gi] > 1;
+            const isRepeat = hist[li][gi].value > 1;
             return (
               <Chip
                 key={`eq-r${ri}-l${li}-g${gi}`}
-                label={`${ladies[li]}-${gents[gi]} ${
-                  isRepeat ? hist[li][gi] : ""
-                }`}
+                label={`${ladies[li]}-${gents[gi]}${
+                  isRepeat ? ` ${hist[li][gi].value}` : ""
+                } - ${hist[li][gi].maxOdds}%`}
                 color={isRepeat ? "primary" : "default"}
               />
             );
@@ -54,7 +73,7 @@ const Equations = (props: EquationsProps) => {
           return !equation.every((e) => e === null) ? (
             <Stack key={`equation-${ri}`} spacing={1}>
               {equation}
-              <Chip label={tempScore} color="success" />
+              <Chip label={tempScore[ri]} color="success" />
             </Stack>
           ) : null;
         })}
