67a6aa1fcd7ac53b5d4201ad7f8e377ba2cc7c9e
Ken Bigler <kennethbigler@gmail.com>
2022-05-17 10:15:08 -0700

    fix: coverage for AYTO is 100, time for subs

--- src/components/games/are-you-the-one/AreYouTheOne.test.tsx
+++ src/components/games/are-you-the-one/AreYouTheOne.test.tsx
@@ -38,12 +38,79 @@ describe("games | are-you-the-one | AreYouTheOne", () => {
     expect(screen.getByText("Matchup 2")).toBeInTheDocument();
   });
 
-  // it("can process a round", async () => {
-  //   render(<AreYouTheOne />);
+  it("can process a round", async () => {
+    render(<AreYouTheOne />);
 
-  //   // select Table Matchups
-  //   fireEvent.click(screen.getByText("A-A"));
-  //   await waitFor(() => TODO: wait for this to change to primary color);
-  //   expect(something?).toBeInTheDocument();
-  // });
+    const AAButton = screen.getByText("A-A");
+    const CDButton = screen.getAllByText("C-D")[0];
+    const CEButton = screen.getAllByText("C-E")[1];
+    const GHButton = screen.getByText("G-H");
+    const HJButton = screen.getAllByText("H-J")[0];
+    const KJButton = screen.getAllByText("K-J")[1];
+    const KM1Button = screen.getAllByText("K-M")[2];
+    const KM2Button = screen.getAllByText("K-M")[5];
+    const SOButton = screen.getAllByText("S-O")[0];
+    const TOButton = screen.getAllByText("T-O")[1];
+    const TTButton = screen.getAllByText("T-T")[1];
+
+    // select Table Matchups
+    expect(AAButton).toHaveClass("MuiButton-outlined");
+    fireEvent.click(AAButton);
+    expect(AAButton).toHaveClass("MuiButton-contained");
+    expect(CDButton).toHaveClass("MuiButton-outlined");
+    fireEvent.click(CDButton);
+    expect(CDButton).toHaveClass("MuiButton-contained");
+    expect(CEButton).toHaveClass("MuiButton-outlined");
+    fireEvent.click(CEButton);
+    expect(CEButton).toHaveClass("MuiButton-contained");
+    expect(GHButton).toHaveClass("MuiButton-outlined");
+    fireEvent.click(GHButton);
+    expect(GHButton).toHaveClass("MuiButton-contained");
+    expect(HJButton).toHaveClass("MuiButton-outlined");
+    fireEvent.click(HJButton);
+    expect(HJButton).toHaveClass("MuiButton-contained");
+    expect(KJButton).toHaveClass("MuiButton-outlined");
+    fireEvent.click(KJButton);
+    expect(KJButton).toHaveClass("MuiButton-contained");
+    expect(KM1Button).toHaveClass("MuiButton-outlined");
+    fireEvent.click(KM1Button);
+    expect(KM1Button).toHaveClass("MuiButton-contained");
+    expect(KM2Button).toHaveClass("MuiButton-outlined");
+    fireEvent.click(KM2Button);
+    expect(KM2Button).toHaveClass("MuiButton-contained");
+    expect(SOButton).toHaveClass("MuiButton-outlined");
+    fireEvent.click(SOButton);
+    expect(SOButton).toHaveClass("MuiButton-contained");
+    expect(TOButton).toHaveClass("MuiButton-outlined");
+    fireEvent.click(TOButton);
+    expect(TOButton).toHaveClass("MuiButton-contained");
+    expect(TTButton).toHaveClass("MuiButton-outlined");
+    fireEvent.click(TTButton);
+    expect(TTButton).toHaveClass("MuiButton-contained");
+    // verify equations were made
+    expect(screen.getByText("Tyranny-Tyler - 0%")).toBeInTheDocument();
+    // verify score updating works
+    fireEvent.change(screen.getByDisplayValue("-1"), {
+      target: { value: "1" },
+    });
+    expect(screen.getByText("Tyranny-Tyler - 9%")).toBeInTheDocument();
+    // open menu
+    fireEvent.click(screen.getByLabelText("select matchup"));
+    expect(screen.getAllByText("Matchup 1")).toHaveLength(3);
+    expect(screen.getByText("Truth Booth")).toBeInTheDocument();
+    // select item
+    fireEvent.click(screen.getByText("Truth Booth"));
+    await waitFor(() =>
+      expect(screen.getAllByText("Matchup 1")).toHaveLength(1)
+    );
+    expect(screen.getByText("Truth Booth")).toBeInTheDocument();
+    // assign a no match
+    fireEvent.click(AAButton);
+    fireEvent.click(screen.getByText("No Match"));
+    expect(screen.getByText("Tyranny-Tyler - 10%")).toBeInTheDocument();
+    // assign a match
+    fireEvent.click(CDButton);
+    fireEvent.click(screen.getByText("Match"));
+    expect(screen.getByText("Tyranny-Tyler - 0%")).toBeInTheDocument();
+  });
 });
