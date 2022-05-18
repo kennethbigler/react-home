932a5e97245dc6df9c2d46ea0bfabea00a6224f9
Ken Bigler <kennethbigler@gmail.com>
2022-05-18 09:09:45 -0700

    fix: update types/react(-dom) and rtl

--- src/components/games/deal-or-no-deal/tests/DealOrNoDeal.test.tsx
+++ src/components/games/deal-or-no-deal/tests/DealOrNoDeal.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { screen, fireEvent } from "@testing-library/react";
+import { screen, fireEvent, waitFor } from "@testing-library/react";
 import render from "../../../../redux-test-render";
 import DealOrNoDeal from "../DealOrNoDeal";
 
@@ -14,82 +14,63 @@ describe("games | deal-or-no-deal | DealOrNoDeal", () => {
     expect(screen.getAllByRole("button")).toHaveLength(26);
   });
 
-  it("plays a game and takes final deal", () => {
-    jest.useFakeTimers();
+  it("plays a game and takes final deal", async () => {
     render(<DealOrNoDeal />);
-    jest.clearAllTimers();
-    jest.runOnlyPendingTimers();
 
     expect(screen.getByText("Your Case: ?")).toBeInTheDocument();
     expect(screen.getByText("Number of Cases to Open: 6")).toBeInTheDocument();
     expect(screen.getByText("Ken: $100")).toBeInTheDocument();
-    expect(screen.getAllByRole("button")).toHaveLength(26);
+    let buttons = screen.getAllByRole("button");
+    expect(buttons).toHaveLength(26);
 
     // select case 7
-    fireEvent.click(screen.getAllByRole("button")[6]);
+    fireEvent.click(buttons[6]);
 
     expect(screen.getByText("Your Case: 7")).toBeInTheDocument();
     expect(screen.getByText("Number of Cases to Open: 6")).toBeInTheDocument();
     expect(screen.getByText("Ken: $0")).toBeInTheDocument();
-    expect(screen.getAllByRole("button")).toHaveLength(26);
 
     // open cases 1-6
-    fireEvent.click(screen.getAllByRole("button")[0]);
-    fireEvent.click(screen.getAllByRole("button")[1]);
-    fireEvent.click(screen.getAllByRole("button")[2]);
-    fireEvent.click(screen.getAllByRole("button")[3]);
-    fireEvent.click(screen.getAllByRole("button")[4]);
-    fireEvent.click(screen.getAllByRole("button")[5]);
+    fireEvent.click(buttons[0]);
+    fireEvent.click(buttons[1]);
+    fireEvent.click(buttons[2]);
+    fireEvent.click(buttons[3]);
+    fireEvent.click(buttons[4]);
+    fireEvent.click(buttons[5]);
     // this one should not open
-    fireEvent.click(screen.getAllByRole("button")[7]);
+    fireEvent.click(buttons[7]);
 
-    jest.runOnlyPendingTimers();
-
-    expect(screen.getAllByText("$1")).toBeDefined();
-    expect(screen.getAllByText("$10")).toBeDefined();
-    expect(screen.getAllByText("$100")).toBeDefined();
-    expect(screen.getAllByText("$1,000")).toBeDefined();
-    expect(screen.getAllByText("$10,000")).toBeDefined();
-    expect(screen.getAllByText("$100,000")).toBeDefined();
-    expect(screen.getAllByText("$1,000,000")).toBeDefined();
-    expect(screen.getByText("Deal")).toBeInTheDocument();
+    await waitFor(() => screen.getByText("Deal"));
     expect(screen.getByText("No Deal")).toBeInTheDocument();
     // there should only be 2 buttons now
     expect(screen.getAllByRole("button")).toHaveLength(2);
+    expect(screen.getAllByText("$1")).toBeDefined();
+    expect(screen.getAllByText("$1,000")).toBeDefined();
+    expect(screen.getAllByText("$1,000,000")).toBeDefined();
 
     fireEvent.click(screen.getByText("No Deal"));
 
-    jest.runOnlyPendingTimers();
-
-    expect(screen.getAllByRole("button")).toHaveLength(26);
-
+    await waitFor(() => expect(screen.getAllByRole("button")).toHaveLength(26));
+    buttons = screen.getAllByRole("button");
     // open cases 8-12
-    fireEvent.click(screen.getAllByRole("button")[7]);
-    fireEvent.click(screen.getAllByRole("button")[8]);
-    fireEvent.click(screen.getAllByRole("button")[9]);
-    fireEvent.click(screen.getAllByRole("button")[10]);
-    fireEvent.click(screen.getAllByRole("button")[11]);
+    fireEvent.click(buttons[7]);
+    fireEvent.click(buttons[8]);
+    fireEvent.click(buttons[9]);
+    fireEvent.click(buttons[10]);
+    fireEvent.click(buttons[11]);
 
-    jest.runOnlyPendingTimers();
-
-    expect(screen.getAllByText("$1")).toBeDefined();
-    expect(screen.getAllByText("$10")).toBeDefined();
-    expect(screen.getAllByText("$100")).toBeDefined();
-    expect(screen.getAllByText("$1,000")).toBeDefined();
-    expect(screen.getAllByText("$10,000")).toBeDefined();
-    expect(screen.getAllByText("$100,000")).toBeDefined();
-    expect(screen.getAllByText("$1,000,000")).toBeDefined();
-    expect(screen.getByText("Deal")).toBeInTheDocument();
+    await waitFor(() => screen.getByText("Deal"));
     expect(screen.getByText("No Deal")).toBeInTheDocument();
     // there should only be 2 buttons now
     expect(screen.getAllByRole("button")).toHaveLength(2);
+    expect(screen.getAllByText("$1")).toBeDefined();
+    expect(screen.getAllByText("$1,000")).toBeDefined();
+    expect(screen.getAllByText("$1,000,000")).toBeDefined();
 
     fireEvent.click(screen.getByText("Deal"));
 
-    jest.runOnlyPendingTimers();
-
+    await waitFor(() => expect(screen.getAllByRole("button")).toHaveLength(27));
     expect(screen.getByText("New Game")).toBeInTheDocument();
-    expect(screen.getAllByRole("button")).toHaveLength(27);
 
     fireEvent.click(screen.getByText("New Game"));
 
