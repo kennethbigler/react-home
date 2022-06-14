8074ec1df1cba174ccac9afeefc0c48c7dd1644f
Ken Bigler <kennethbigler@gmail.com>
2022-06-14 09:10:26 -0700

    fix: update map

--- src/components/resume/travel-map/tests/TravelMap.test.tsx
+++ src/components/resume/travel-map/tests/TravelMap.test.tsx
@@ -4,13 +4,9 @@ import TravelMap from "../TravelMap";
 
 describe("resume | travel-map | TravelMap", () => {
   it("renders as expected", () => {
-    const { container } = render(<TravelMap />);
+    render(<TravelMap />);
 
     expect(screen.getByText("My Travel Map")).toBeInTheDocument();
-    expect(container.querySelector(".rsm-svg")).toBeInTheDocument();
-    expect(container.querySelector(".rsm-zoomable-group")).toBeInTheDocument();
-    expect(container.querySelector("#rsm-sphere")).toBeInTheDocument();
-    expect(container.querySelector(".rsm-geographies")).toBeInTheDocument();
     expect(
       screen.getByText("I have been to 32 countries:")
     ).toBeInTheDocument();
