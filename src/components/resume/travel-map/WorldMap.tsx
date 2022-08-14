9d771a533087883d05dd393ee836f394c63c24ba
Ken Bigler <kennethbigler@gmail.com>
2022-08-14 12:44:54 -0700

    fix: change map back

--- src/components/resume/travel-map/WorldMap.tsx
+++ src/components/resume/travel-map/WorldMap.tsx
@@ -1,28 +1,151 @@
 import React from "react";
-import { useRecoilState } from "recoil";
-import Map, { DataItem } from "react-svg-worldmap";
-import { blueGrey } from "@mui/material/colors";
+import {
+  ComposableMap,
+  ZoomableGroup,
+  Geographies,
+  Geography,
+  Sphere,
+} from "react-simple-maps";
+import { blueGrey, red } from "@mui/material/colors";
+import Popover from "./Popover";
 import countries from "../../../constants/travel";
-import themeAtom from "../../../recoil/theme-atom";
 
-const data: DataItem[] = Object.keys(countries).map((k) => ({
-  country: countries[k].code,
-  value: 1,
-}));
+interface GeographyType {
+  type: "Feature";
+  rsmKey: string;
+  svgPath: string;
+  geometry: {
+    type: "Polygon" | "MultiPolygon";
+    coordinates: [number, number][];
+  };
+  properties: {
+    NAME: string;
+    NAME_LONG: string;
+    ABBREV: string;
+    FORMAL_EN: string;
+    POP_EST: number;
+    POP_RANK: number;
+    GDP_MD_EST: number;
+    POP_YEAR: number;
+    GDP_YEAR: number;
+    ISO_A2: string;
+    ISO_A3: string;
+    CONTINENT: string;
+    REGION_UN: string;
+    SUBREGION: string;
+  };
+}
 
-const WorldMap = () => {
-  const [theme] = useRecoilState(themeAtom);
-  const screenWidth = Math.min(document.body.clientWidth - 32, 2000);
+type HandleEnter = (
+  geography: GeographyType
+) => (evt: React.MouseEvent<SVGPathElement, MouseEvent>) => void;
+
+interface WorldMapHook {
+  x: number;
+  y: number;
+  content: string;
+  hide: boolean;
+  handleEnter: HandleEnter;
+  handleLeave: () => void;
+}
+
+const STROKE = blueGrey[900];
+const HOVER = blueGrey[500];
+const VISITED_HOVER = red[800];
+const FILL = blueGrey[100];
+const PRESSED = blueGrey[800];
+const RATIO = 100 / 465.33;
+
+const defaultStyle: React.CSSProperties = {
+  stroke: STROKE,
+  strokeWidth: 0.75,
+  outline: "none",
+};
+
+function useWorldMap(): WorldMapHook {
+  const [x, setX] = React.useState(0);
+  const [y, setY] = React.useState(0);
+  const [content, setContent] = React.useState("");
+  const [hide, setHide] = React.useState(true);
+
+  const handleEnter: HandleEnter =
+    (geography) =>
+    (evt): void => {
+      const name = geography.properties.NAME || "";
+      setX(evt.clientX);
+      setY(evt.clientY + window.pageYOffset);
+      setContent(`${name} ${countries[name] ? countries[name].flag : ""}`);
+      setHide(false);
+    };
+
+  const handleLeave = (): void => {
+    setHide(true);
+  };
+
+  return {
+    x,
+    y,
+    content,
+    hide,
+    handleEnter,
+    handleLeave,
+  };
+}
+
+const WorldMap = React.memo(() => {
+  const { x, y, content, hide, handleEnter, handleLeave } = useWorldMap();
+
+  const screenWidth = document.body.clientWidth - 32;
 
   return (
-    <Map
-      color={blueGrey[500]}
-      data={data}
-      size={screenWidth}
-      backgroundColor="transparent"
-      borderColor={theme.mode === "dark" ? "white" : "black"}
-    />
+    <>
+      <ComposableMap
+        width={screenWidth}
+        height={(screenWidth * 546) / 744}
+        projectionConfig={{ scale: screenWidth * RATIO }}
+      >
+        <ZoomableGroup>
+          <Sphere
+            id="rsm-sphere"
+            stroke={FILL}
+            strokeWidth={2}
+            fill="transparent"
+          />
+          <Geographies geography="/world-110m.json">
+            {({ geographies }) =>
+              geographies.map((geo: GeographyType) => (
+                <Geography
+                  key={geo.rsmKey}
+                  geography={geo}
+                  onMouseEnter={handleEnter(geo)}
+                  onMouseLeave={handleLeave}
+                  style={{
+                    default: {
+                      fill: countries[geo.properties.NAME]
+                        ? countries[geo.properties.NAME].color
+                        : FILL,
+                      ...defaultStyle,
+                    },
+                    hover: {
+                      fill: countries[geo.properties.NAME]
+                        ? VISITED_HOVER
+                        : HOVER,
+                      ...defaultStyle,
+                    },
+                    pressed: {
+                      fill: PRESSED,
+                      ...defaultStyle,
+                    },
+                  }}
+                />
+              ))
+            }
+          </Geographies>
+        </ZoomableGroup>
+      </ComposableMap>
+      <Popover x={x} y={y} hide={hide} content={content} />
+    </>
   );
-};
+});
 
 export default WorldMap;
