3cacb7f1d2eb3ec88f6a3e531bcaf17a3fe71c96
Ken Bigler <kennethbigler@gmail.com>
2023-02-13 15:04:04 -0800

    fix: trying explicit ghtoken

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,14 +1,15 @@
-{
-  "ci": {
-    "collect": {
-      "staticDistDir": "./build"
+module.exports = {
+  ci: {
+    collect: {
+      staticDistDir: "./build"
     },
-    "upload": {
-      "target": "temporary-public-storage"
+    upload: {
+      githubToken: "$LHCI_GITHUB_APP_TOKEN",
+      target: "temporary-public-storage"
     },
-    "assert": {
-      "preset": "lighthouse:recommended",
-      "assertions": {
+    assert: {
+      preset: "lighthouse:recommended",
+      assertions: {
         "csp-xss": "warn",
         "non-composited-animations": "warn",
         "preload-lcp-image": "warn",
@@ -18,4 +19,4 @@
       }
     }
   }
-}
+};
