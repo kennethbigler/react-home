14c4f8c014a8e0728dc7a1a81edefa9f3966c87a
Ken Bigler <kennethbigler@gmail.com>
2023-04-21 15:16:49 -0700

    fix: upgrade lhci version

--- src/components/resume/summary/Info.tsx
+++ src/components/resume/summary/Info.tsx
@@ -24,58 +24,84 @@ const handleClick = (): void => {
   window.open("https://www.linkedin.com/in/kennethbigler", "linkedIn");
 };
 
-const Info: React.FC = React.memo(() => (
-  <Grid container spacing={1}>
-    <Grid item md={4} xs={12}>
-      <img
-        alt="Kenneth Bigler"
-        onClick={handleClick}
-        src={photo}
-        style={imageStyles}
-      />
+const preloadImage = (src: string) =>
+  new Promise((resolve, reject) => {
+    const img = new Image();
+    img.onload = () => resolve(img);
+    img.onerror = () => reject(src);
+    img.onabort = () => reject(src);
+    img.src = src;
+  });
+
+const Info: React.FC = () => {
+  const [assetsLoaded, setAssetsLoaded] = React.useState<boolean>(false);
+
+  React.useEffect(() => {
+    const effect = async () => {
+      await preloadImage(photo);
+      setAssetsLoaded(true);
+    };
+    effect().catch(() => console.error("failed to load"));
+  }, []);
+
+  return (
+    <Grid container spacing={1}>
+      <Grid item md={4} xs={12}>
+        {assetsLoaded ? (
+          <img
+            alt="Kenneth Bigler"
+            onClick={handleClick}
+            src={photo}
+            style={imageStyles}
+          />
+        ) : (
+          <p>image loading...</p>
+        )}
+      </Grid>
+      <Grid item md={8} xs={12}>
+        <ExpandableCard title={getJob(workExp[0])}>
+          <Table>
+            <TableBody>
+              <TableRow>
+                <TableCell>Location</TableCell>
+                <TableCell>Mountain View, CA</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>Role</TableCell>
+                <TableCell>Accessibility Engineer Leader</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>Volunteer Work</TableCell>
+                <TableCell>Second Harvest Food Bank</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell colSpan={2} align="center">
+                  Licenses
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>
+                  <p>Driver License (C &amp; M1)</p>
+                  <p>GoKart Racing License</p>
+                  <p>Range Safety Certified</p>
+                  <p>
+                    CPR, AED, &amp; First Aid Certified (Adult, Child, &amp;
+                    Baby)
+                  </p>
+                </TableCell>
+                <TableCell>
+                  <p>Ordained Minister</p>
+                  <p>Former Magician&apos;s Assistant</p>
+                  <p>Former Salsa Dance Instructor</p>
+                  <p>Former Salsa Dance Choreographer</p>
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </ExpandableCard>
+      </Grid>
     </Grid>
-    <Grid item md={8} xs={12}>
-      <ExpandableCard title={getJob(workExp[0])}>
-        <Table>
-          <TableBody>
-            <TableRow>
-              <TableCell>Location</TableCell>
-              <TableCell>Mountain View, CA</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>Role</TableCell>
-              <TableCell>Accessibility Engineer Leader</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>Volunteer Work</TableCell>
-              <TableCell>Second Harvest Food Bank</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell colSpan={2} align="center">
-                Licenses
-              </TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>
-                <p>Driver License (C &amp; M1)</p>
-                <p>GoKart Racing License</p>
-                <p>Range Safety Certified</p>
-                <p>
-                  CPR, AED, &amp; First Aid Certified (Adult, Child, &amp; Baby)
-                </p>
-              </TableCell>
-              <TableCell>
-                <p>Ordained Minister</p>
-                <p>Former Magician&apos;s Assistant</p>
-                <p>Former Salsa Dance Instructor</p>
-                <p>Former Salsa Dance Choreographer</p>
-              </TableCell>
-            </TableRow>
-          </TableBody>
-        </Table>
-      </ExpandableCard>
-    </Grid>
-  </Grid>
-));
+  );
+};
 
 export default Info;
