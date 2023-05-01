6e16bc48c7f62b057b8a27d29bc88fa91da7d7ce
Ken Bigler <ken_bigler@intuit.com>
2023-05-01 17:01:14 -0400

    fix: tests

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: { open: true, port: 3000 },
+  server: { open: true },
   test: {
     globals: true,
     environment: 'jsdom',
