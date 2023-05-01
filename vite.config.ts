c3a65103bbb1700351364b4aa640cf8b92ef6ebc
Ken Bigler <ken_bigler@intuit.com>
2023-05-01 15:43:31 -0400

    fix: port number

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: { open: true },
+  server: { open: true, port: 3000 },
   test: {
     globals: true,
     environment: 'jsdom',
