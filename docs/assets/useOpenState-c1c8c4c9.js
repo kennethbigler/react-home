15dac86547d73814d4f77def306cad98ade5d452
Ken Bigler <ken_bigler@intuit.com>
2023-05-02 07:57:40 -0400

    feat: deploy with new bundler

---
+++ docs/assets/useOpenState-c1c8c4c9.js
@@ -0,0 +1,2 @@
+import{r as n}from"./index-2cf40ff9.js";const l=(s=!1)=>{const[t,e]=n.useState(s);return[t,()=>{e(!0)},()=>{e(!1)}]};export{l as u};
+//# sourceMappingURL=useOpenState-c1c8c4c9.js.map
