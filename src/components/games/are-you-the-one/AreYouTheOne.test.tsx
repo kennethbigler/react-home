ab1b2b3a4cb852cf8390a19a8411f057e9204575
Ken Bigler <ken_bigler@intuit.com>
2023-05-02 14:30:26 -0400

    fix: test blackout

--- src/components/games/are-you-the-one/AreYouTheOne.test.tsx
+++ src/components/games/are-you-the-one/AreYouTheOne.test.tsx
@@ -122,6 +122,82 @@ describe("games | are-you-the-one | AreYouTheOne", () => {
     expect(screen.getByText("Tyranny-Tyler - 0%")).toBeInTheDocument();
   });
 
+  it("can blackout", async () => {
+    render(<AreYouTheOne />);
+
+    // Set Season to 5 (so I don't need to update tests)
+    expect(screen.getByLabelText("select season")).toBeInTheDocument();
+    expect(screen.getByText("Season 9")).toBeInTheDocument();
+    expect(screen.queryByText("Season 5")).toBeNull();
+    // open menu
+    fireEvent.click(screen.getByLabelText("select season"));
+    expect(screen.getAllByText("Season 9")).toHaveLength(2);
+    expect(screen.getByText("Season 5")).toBeInTheDocument();
+    // select item
+    fireEvent.click(screen.getByText("Season 5"));
+    await waitFor(() => expect(screen.queryByText("Season 9")).toBeNull());
+    expect(screen.getByText("Season 5")).toBeInTheDocument();
+
+    const AAButton = screen.getByText("A-A");
+    const CDButton = screen.getAllByText("C-D")[0];
+    const CEButton = screen.getAllByText("C-E")[1];
+    const GHButton = screen.getByText("G-H");
+    const HJButton = screen.getAllByText("H-J")[0];
+    const KJButton = screen.getAllByText("K-J")[1];
+    const KM1Button = screen.getAllByText("K-M")[2];
+    const KM2Button = screen.getAllByText("K-M")[5];
+    const SOButton = screen.getAllByText("S-O")[0];
+    const TOButton = screen.getAllByText("T-O")[1];
+    const TTButton = screen.getAllByText("T-T")[1];
+
+    // select Table Matchups
+    expect(AAButton).toHaveClass("MuiButton-outlined");
+    fireEvent.click(AAButton);
+    expect(AAButton).toHaveClass("MuiButton-contained");
+    expect(CDButton).toHaveClass("MuiButton-outlined");
+    fireEvent.click(CDButton);
+    expect(CDButton).toHaveClass("MuiButton-contained");
+    expect(CEButton).toHaveClass("MuiButton-outlined");
+    fireEvent.click(CEButton);
+    expect(CEButton).toHaveClass("MuiButton-contained");
+    expect(GHButton).toHaveClass("MuiButton-outlined");
+    fireEvent.click(GHButton);
+    expect(GHButton).toHaveClass("MuiButton-contained");
+    expect(HJButton).toHaveClass("MuiButton-outlined");
+    fireEvent.click(HJButton);
+    expect(HJButton).toHaveClass("MuiButton-contained");
+    expect(KJButton).toHaveClass("MuiButton-outlined");
+    fireEvent.click(KJButton);
+    expect(KJButton).toHaveClass("MuiButton-contained");
+    expect(KM1Button).toHaveClass("MuiButton-outlined");
+    fireEvent.click(KM1Button);
+    expect(KM1Button).toHaveClass("MuiButton-contained");
+    expect(KM2Button).toHaveClass("MuiButton-outlined");
+    fireEvent.click(KM2Button);
+    expect(KM2Button).toHaveClass("MuiButton-contained");
+    expect(SOButton).toHaveClass("MuiButton-outlined");
+    fireEvent.click(SOButton);
+    expect(SOButton).toHaveClass("MuiButton-contained");
+    expect(TOButton).toHaveClass("MuiButton-outlined");
+    fireEvent.click(TOButton);
+    expect(TOButton).toHaveClass("MuiButton-contained");
+    expect(TTButton).toHaveClass("MuiButton-outlined");
+    fireEvent.click(TTButton);
+    expect(TTButton).toHaveClass("MuiButton-contained");
+    fireEvent.click(screen.getByText("Blackout"));
+    expect(AAButton).toHaveClass("MuiButton-containedError");
+    expect(CDButton).toHaveClass("MuiButton-containedError");
+    expect(CEButton).toHaveClass("MuiButton-containedError");
+    expect(GHButton).toHaveClass("MuiButton-containedError");
+    expect(HJButton).toHaveClass("MuiButton-containedError");
+    expect(KJButton).toHaveClass("MuiButton-containedError");
+    expect(KM1Button).toHaveClass("MuiButton-containedError");
+    expect(KM2Button).toHaveClass("MuiButton-containedError");
+    expect(SOButton).toHaveClass("MuiButton-containedError");
+    expect(TOButton).toHaveClass("MuiButton-containedError");
+    expect(TTButton).toHaveClass("MuiButton-containedError");
+  });
+
   it("can change couples to match or no match via truth booth", async () => {
     render(<AreYouTheOne />);
 
