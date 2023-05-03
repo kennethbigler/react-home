b05131667e6672879bb08a955e7c315996a7128d
Ken Bigler <ken_bigler@intuit.com>
2023-05-02 22:34:12 -0400

    fix: deploy script then deploy

---
+++ docs/assets/index-32f5a2bb.js
@@ -0,0 +1,2 @@
+import{r as s,j as e}from"./index-cb293786.js";import{T as t}from"./MenuItem-f1629f13.js";const r="/assets/kenneth_bigler_resume-a5164e45.png",a={maxWidth:1275,width:"100%",display:"block",margin:"auto"},m=s.memo(()=>e.jsxs(e.Fragment,{children:[e.jsx(t,{variant:"h2",component:"h1",children:"Resume"}),e.jsx("img",{src:r,alt:"Kenneth Bigler Software Engineer Resume",style:a})]})),i=m;export{i as default};
+//# sourceMappingURL=index-32f5a2bb.js.map
