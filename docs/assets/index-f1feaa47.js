18d7b71968d11a38acaeb1b63068253749fc647a
Ken Bigler <ken_bigler@intuit.com>
2023-05-25 10:07:02 -0700

    fix: deploy lazy rules

--- docs/assets/index-f1feaa47.js
+++
@@ -1,2 +0,0 @@
-import{r as s,j as e}from"./index-f6f0a7d4.js";import{T as t}from"./MenuItem-94bd3390.js";const r="/assets/kenneth_bigler_resume-a5164e45.png",a={maxWidth:1275,width:"100%",display:"block",margin:"auto"},m=s.memo(()=>e.jsxs(e.Fragment,{children:[e.jsx(t,{variant:"h2",component:"h1",children:"Resume"}),e.jsx("img",{src:r,alt:"Kenneth Bigler Software Engineer Resume",style:a})]})),i=m;export{i as default};
-//# sourceMappingURL=index-f1feaa47.js.map
