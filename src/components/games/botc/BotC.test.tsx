81828534d9256b86a208251c8b88dd4d9331d6d7
Ken Bigler <ken_bigler@intuit.com>
2023-10-18 13:32:23 -0700

    test: botc

--- src/components/games/botc/BotC.test.tsx
+++ src/components/games/botc/BotC.test.tsx
@@ -1,11 +1,29 @@
-import { screen } from "@testing-library/react";
+import { screen, fireEvent } from "@testing-library/react";
 import render from "../../../recoil-test-render";
 import BotC from "./BotC";
 
 describe("games | BotC", () => {
-  it("renders as expected", () => {
+  it("renders as expected", async () => {
     render(<BotC />);
 
     expect(screen.getByText("BotC")).toBeInTheDocument();
+    // Press Players button then close
+    fireEvent.click(screen.getByText("Players"));
+    expect(screen.getByText("Close")).toBeInTheDocument();
+    fireEvent.click(screen.getByText("Close"));
+    // Press Roles button (for Player Dashboard)
+    fireEvent.click(screen.getAllByText("Roles")[0]);
+    expect(screen.getByText("Chef")).toBeInTheDocument();
+    expect(screen.getByText("Monk")).toBeInTheDocument();
+    // Select 2 roles
+    expect(screen.getAllByText("Chef")).toHaveLength(1);
+    fireEvent.click(screen.getByText("Chef"));
+    expect(screen.getAllByText("Chef")).toHaveLength(2);
+    fireEvent.click(screen.getByText("Monk"));
+    expect(screen.getAllByText("Chef")).toHaveLength(1);
+    expect(screen.getByText("Chef, Monk")).toBeInTheDocument();
+    // Deselect a role
+    fireEvent.click(screen.getByText("Monk"));
+    expect(screen.getAllByText("Chef")).toHaveLength(2);
   });
 });
