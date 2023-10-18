081615d8d06c3d4719ac4d4eff212eae18757ce2
Ken Bigler <ken_bigler@intuit.com>
2023-10-18 14:07:25 -0700

    fix: linters

--- src/components/games/botc/BotC.test.tsx
+++ src/components/games/botc/BotC.test.tsx
@@ -3,7 +3,7 @@ import render from "../../../recoil-test-render";
 import BotC from "./BotC";
 
 describe("games | BotC", () => {
-  it("renders as expected", async () => {
+  it("renders as expected", () => {
     render(<BotC />);
 
     expect(screen.getByText("BotC")).toBeInTheDocument();
