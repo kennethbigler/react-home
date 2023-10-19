cabe168aa5f0e567f4bdc497069e3d731347dfda
Ken Bigler <ken_bigler@intuit.com>
2023-10-19 13:57:48 -0700

    fix: source-map explorer

--- scripts/analyze.sh
+++ scripts/analyze.sh
@@ -9,7 +9,7 @@ buildApp
 echo $LINE
 echo "Running source-map-explorer..."
 echo $LINE
-npx source-map-explorer "dist/assets/*.js" || throwError "source-map"
+npx source-map-explorer "dist/assets/*.js" --no-border-checks || throwError "source-map"
 echo $LINE
 echo "Cleaning up files..."
 rm -rf dist || throwError "cleanup"
