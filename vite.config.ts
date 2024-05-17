f52ce03fec926df8de842c7380f3c166063eb4c1
Ken Bigler <Ken_Bigler@intuit.com>
2024-05-17 12:01:05 -0400

    fix: sourcemap

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,16 @@
+// use test types instead of default types
 /// <reference types="vitest" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // for lighthouse
+  build: { sourcemap: true },
   plugins: [react()],
+  // open up browser on start
   server: { open: true },
+  // test configs
   test: {
     globals: true,
     environment: "jsdom",
