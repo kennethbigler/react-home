2b77495a83d9db001dcef9b624a98fb81720d23b
Ken Bigler <Ken_Bigler@intuit.com>
2024-05-17 11:23:06 -0400

    fix: vitest configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vitest" />
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: { sourcemap: true },
   plugins: [react()],
   server: { open: true },
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: ['src/setupTests.ts'],
+    environment: "jsdom",
+    setupFiles: ["src/setupTests.ts"],
     coverage: {
-      reporter: ['text', 'html', 'json'],
-      include: ['src/**'],
-      statements: 90,
-      branches: 85,
-      lines: 90,
-      functions: 90
+      provider: "v8",
+      reporter: ["text", "html", "json"],
+      include: ["src/**"],
+      thresholds: {
+        statements: 90,
+        branches: 85,
+        lines: 90,
+        functions: 80,
+      },
     },
   },
 });
