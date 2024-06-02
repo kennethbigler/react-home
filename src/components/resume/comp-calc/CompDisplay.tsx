4205ac5d1ec797522c594bf19b662254457c4bdd
Ken Bigler <kennethbigler@gmail.com>
2024-06-01 17:17:46 -0700

    fix: enable entries

--- src/components/resume/comp-calc/CompDisplay.tsx
+++ src/components/resume/comp-calc/CompDisplay.tsx
@@ -1,67 +1,57 @@
 import * as React from "react";
-import { useRecoilValue } from "recoil";
 import { Box, Typography } from "@mui/material";
 import {
+  CompCalcEntry,
   CompEntry,
-  compCalcReadOnlyState,
 } from "../../../recoil/comp-calculator-state";
+import dateObj from "../../../apis/DateHelper";
 
 interface CompEntryProps {
   compEntries: CompEntry[];
+  compCalcEntries: CompCalcEntry[];
 }
 
-const CompDisplay: React.FC<CompEntryProps> = ({ compEntries }) => {
-  const compCalcEntries = useRecoilValue(compCalcReadOnlyState);
-
-  return (
-    <>
-      <Typography variant="h2" component="h1">
-        Comp Calculator
-      </Typography>
-
-      {compEntries.map((compEntry, i) => {
-        const {
-          entryDate,
-          salary,
-          bonus,
-          priceNow,
-          priceThen,
-          grantDuration,
-          grantQty,
-        } = compEntry;
-        const {
-          stock,
-          stockAdj,
-          total,
-          totalAdj,
-          netDiff,
-          grantThen,
-          grantNow,
-        } = compCalcEntries[i];
-        return (
-          <Box key={`comp-calc-entry-${i}`}>
-            <Typography variant="h3">SALARY</Typography>
-            <Typography>Date - {entryDate.format("MMMM Y")}</Typography>
-            <Typography>Salary - ${salary}</Typography>
-            <Typography>Bonus - ${bonus}</Typography>
-            <Typography>Stock (Adj) - ${stockAdj} *</Typography>
-            <Typography>Stock - ${stock} *</Typography>
-            <Typography>Total - ${total} *</Typography>
-            <Typography>Total (Adj) - ${totalAdj} *</Typography>
-            <Typography>Net - ${netDiff} *</Typography>
-            <br />
-            <Typography variant="h3">STOCK</Typography>
-            <Typography>Price Now - ${priceNow}</Typography>
-            <Typography>Price Then - ${priceThen}</Typography>
-            <Typography>Grant Qty - {grantQty} stocks</Typography>
-            <Typography>Grant Duration - {grantDuration} years</Typography>
-            <Typography>Grant Then - ${grantThen} *</Typography>
-            <Typography>Grant Now - ${grantNow} *</Typography>
-          </Box>
-        );
-      })}
-    </>
-  );
-};
+const CompDisplay: React.FC<CompEntryProps> = ({
+  compEntries,
+  compCalcEntries,
+}) => (
+  <>
+    {compEntries.map((compEntry, i) => {
+      const {
+        entryDate,
+        salary,
+        bonus,
+        priceNow,
+        priceThen,
+        grantDuration,
+        grantQty,
+      } = compEntry;
+      const { stock, stockAdj, total, totalAdj, netDiff, grantThen, grantNow } =
+        compCalcEntries[i];
+      console.log(entryDate);
+      return (
+        <Box key={`comp-calc-entry-${i}`}>
+          <Typography variant="h3">SALARY</Typography>
+          <Typography>Date - {dateObj(entryDate).format("MMMM Y")}</Typography>
+          <Typography>Salary - ${salary}</Typography>
+          <Typography>Bonus - ${bonus}</Typography>
+          <Typography>Stock (Adj) - ${stockAdj} *</Typography>
+          <Typography>Stock - ${stock} *</Typography>
+          <Typography>Total - ${total} *</Typography>
+          <Typography>Total (Adj) - ${totalAdj} *</Typography>
+          <Typography>Net - ${netDiff} *</Typography>
+          <br />
+          <Typography variant="h3">STOCK</Typography>
+          <Typography>Price Now - ${priceNow}</Typography>
+          <Typography>Price Then - ${priceThen}</Typography>
+          <Typography>Grant Qty - {grantQty} stocks</Typography>
+          <Typography>Grant Duration - {grantDuration} years</Typography>
+          <Typography>Grant Then - ${grantThen} *</Typography>
+          <Typography>Grant Now - ${grantNow} *</Typography>
+        </Box>
+      );
+    })}
+  </>
+);
 
 export default CompDisplay;
