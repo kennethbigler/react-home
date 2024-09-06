068604eef6328708df80e28eb3981f90a5c7147f
Ken Bigler <Ken_Bigler@intuit.com>
2024-09-06 10:05:04 -0700

    fix: move package into eslintrc

--- src/helpers/asyncForEach.test.ts
+++ src/helpers/asyncForEach.test.ts
@@ -3,7 +3,7 @@ import asyncForEach from "./asyncForEach";
 describe("helpers | asyncForEach", () => {
   it("works as expected", async () => {
     let x = 1;
-    // eslint-disable-next-line @typescript-eslint/require-await
+
     await asyncForEach([1, 2, 3], async (num) => {
       x += num;
     });
