2e14b479650cc3ac5100fe92ed1d1da44fec0b4e
Ken Bigler <Ken_Bigler@intuit.com>
2024-10-31 09:48:20 -0700

    fix: semantic html

--- src/components/games/HomeMenuItem.tsx
+++ src/components/games/HomeMenuItem.tsx
@@ -2,6 +2,7 @@ import Typography from "@mui/material/Typography";
 import Avatar from "@mui/material/Avatar";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
+import ButtonBase from "@mui/material/ButtonBase";
 import { MenuItem } from "./menu-items";
 
 export interface HomeSectionProps {
@@ -10,41 +11,47 @@ export interface HomeSectionProps {
   onClick?: (loc: string) => void;
 }
 
+const subHeaderStyles: React.CSSProperties = {
+  textAlign: "left",
+  marginBottom: 40,
+};
 const menuWrapperStyles: React.CSSProperties = {
   display: "flex",
   justifyContent: "space-around",
   flexWrap: "wrap",
 };
-const dividerStyles: React.CSSProperties = { marginTop: 40 };
-const subHeaderStyles: React.CSSProperties = {
-  textAlign: "left",
-  marginBottom: 40,
+const cardStyles: React.CSSProperties = {
+  width: 250,
+  cursor: "pointer",
+  marginBottom: "20px",
+};
+const avatarStyles: React.CSSProperties = {
+  margin: "auto",
+  marginTop: 30,
+  color: "white",
 };
 
 const HomeSection = ({ items, title, onClick }: HomeSectionProps) => (
   <>
-    <hr style={dividerStyles} />
+    <hr style={{ marginTop: 40 }} />
     <Typography variant="h4" component="h3" style={subHeaderStyles}>
       {title}
     </Typography>
     <div style={menuWrapperStyles}>
       {items.map(({ name, route, icon }: MenuItem) => (
-        <Card
-          sx={{ width: 250, cursor: "pointer", marginBottom: "20px" }}
-          role="link"
-          tabIndex={0}
+        <ButtonBase
           key={name}
           onClick={(): void => onClick && onClick(`/games/${route || ""}`)}
         >
-          <CardContent>
-            <Typography variant="h5" component="h4" textAlign="center">
-              {name}
-            </Typography>
-            <Avatar style={{ margin: "auto", marginTop: 30, color: "white" }}>
-              {icon}
-            </Avatar>
-          </CardContent>
-        </Card>
+          <Card sx={cardStyles}>
+            <CardContent>
+              <Typography variant="h5" component="h4" textAlign="center">
+                {name}
+              </Typography>
+              <Avatar style={avatarStyles}>{icon}</Avatar>
+            </CardContent>
+          </Card>
+        </ButtonBase>
       ))}
     </div>
   </>
