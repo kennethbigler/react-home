f880a8d3cb9f8b1d8b08e97d73373bff1df11500
Ken Bigler <kennethbigler@gmail.com>
2024-11-04 20:52:58 -0800

    feat: add universal tracker

--- src/components/games/botc/edit-players/EditPlayers.tsx
+++ src/components/games/botc/edit-players/EditPlayers.tsx
@@ -1,11 +1,8 @@
 import * as React from "react";
 import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid2";
-import Snackbar from "@mui/material/Snackbar";
-import Alert from "@mui/material/Alert";
 import Slider from "@mui/material/Slider";
 import { SelectChangeEvent } from "@mui/material";
-import InfoPopup from "../../../common/info-popover/InfoPopup";
 import {
   BotCPlayer,
   BOTC_MAX_PLAYERS,
@@ -23,7 +20,7 @@ interface EditPlayersProps {
   numPlayers: number;
   numTravelers: number;
   script: number;
-  newBotCGame: () => void;
+  handleReset: () => void;
   updateNames: (i: number) => (e: React.FocusEvent<HTMLInputElement>) => void;
   updateNumPlayers: (_e: Event, value: number | number[]) => void;
   updateNumTravelers: (_e: Event, value: number | number[]) => void;
@@ -38,87 +35,62 @@ const EditPlayers = ({
   numPlayers,
   numTravelers,
   script,
-  newBotCGame,
+  handleReset,
   updateNames,
   updateNumPlayers,
   updateNumTravelers,
   updatePlayerOrder,
   updateScript,
   updateText,
-}: EditPlayersProps) => {
-  const [hasToast, setHasToast] = React.useState(false);
-  /** close the toast message */
-  const handleClose = () => setHasToast(false);
-  /** reset the BOTC game and open the success toast */
-  const handleReset = () => {
-    newBotCGame();
-    setHasToast(true);
-  };
+}: EditPlayersProps) => (
+  <Grid container spacing={1}>
+    <Grid size={12} sx={{ textAlign: "center" }}>
+      <ScriptSelect script={script} updateScript={updateScript} />
+      <ScriptControls
+        isText={isText}
+        updateText={updateText}
+        handleReset={handleReset}
+      />
+    </Grid>
 
-  return (
-    <div className="flex-container">
-      <Typography variant="h2" component="h1" gutterBottom>
-        BotC
+    <Grid size={12}>
+      <Typography>
+        Players: {numPlayers} / Dist: {playerDist[numPlayers]}
       </Typography>
+      <Slider
+        aria-label="player count"
+        min={BOTC_MIN_PLAYERS}
+        max={BOTC_MAX_PLAYERS}
+        value={numPlayers}
+        onChange={updateNumPlayers}
+      />
+    </Grid>
 
-      <InfoPopup title="Players">
-        <Grid container spacing={1}>
-          <Grid size={12} sx={{ textAlign: "center" }}>
-            <ScriptSelect script={script} updateScript={updateScript} />
-            <ScriptControls
-              isText={isText}
-              updateText={updateText}
-              handleReset={handleReset}
-            />
-          </Grid>
+    <Grid size={12}>
+      <Typography>Travelers: {numTravelers}</Typography>
+      <Slider
+        aria-label="traveler count"
+        min={0}
+        max={BOTC_MAX_TRAVELERS}
+        value={numTravelers}
+        onChange={updateNumTravelers}
+      />
+    </Grid>
 
-          <Grid size={12}>
-            <Typography>
-              Players: {numPlayers} / Dist: {playerDist[numPlayers]}
-            </Typography>
-            <Slider
-              aria-label="player count"
-              min={BOTC_MIN_PLAYERS}
-              max={BOTC_MAX_PLAYERS}
-              value={numPlayers}
-              onChange={updateNumPlayers}
-            />
-          </Grid>
-
-          <Grid size={12}>
-            <Typography>Travelers: {numTravelers}</Typography>
-            <Slider
-              aria-label="traveler count"
-              min={0}
-              max={BOTC_MAX_TRAVELERS}
-              value={numTravelers}
-              onChange={updateNumTravelers}
-            />
-          </Grid>
-
-          {botcPlayers.map((player, i) =>
-            i < numPlayers + numTravelers ? (
-              <EditNameAndPos
-                key={`player${i}-${player.name}`}
-                first={i === 0}
-                last={i === numPlayers + numTravelers - 1}
-                name={player.name}
-                moveUp={updatePlayerOrder(i, -1)}
-                moveDown={updatePlayerOrder(i, 1)}
-                onBlur={updateNames(i)}
-              />
-            ) : null,
-          )}
-        </Grid>
-      </InfoPopup>
-
-      <Snackbar open={hasToast} autoHideDuration={3000} onClose={handleClose}>
-        <Alert onClose={handleClose} severity="success" sx={{ width: "100%" }}>
-          Game Reset
-        </Alert>
-      </Snackbar>
-    </div>
-  );
-};
+    {botcPlayers.map((player, i) =>
+      i < numPlayers + numTravelers ? (
+        <EditNameAndPos
+          key={`player${i}-${player.name}`}
+          first={i === 0}
+          last={i === numPlayers + numTravelers - 1}
+          name={player.name}
+          moveUp={updatePlayerOrder(i, -1)}
+          moveDown={updatePlayerOrder(i, 1)}
+          onBlur={updateNames(i)}
+        />
+      ) : null,
+    )}
+  </Grid>
+);
 
 export default EditPlayers;
--- src/components/games/botc/player-notes/PlayerNotes.tsx
+++ src/components/games/botc/player-notes/PlayerNotes.tsx
@@ -1,12 +1,10 @@
 import Grid from "@mui/material/Grid2";
-import Typography from "@mui/material/Typography";
 import PlayerCard from "./PlayerCard";
 import {
   BotCPlayer,
   BotCPlayerStatus,
   BotCRole,
 } from "../../../../recoil/botc-atom";
-import { playerDist } from "../../../../constants/botc";
 
 interface PlayerNotesProps {
   botcPlayers: BotCPlayer[];
@@ -34,13 +32,6 @@ const PlayerNotes = ({
   updateStats,
 }: PlayerNotesProps) => (
   <Grid container spacing={2}>
-    <Grid size={12}>
-      <Typography>
-        Dist: {playerDist[numPlayers]}
-        {numTravelers ? ` +${numTravelers}` : ""}
-      </Typography>
-    </Grid>
-
     {botcPlayers.map((player, i) =>
       i < numPlayers + numTravelers ? (
         <PlayerCard
