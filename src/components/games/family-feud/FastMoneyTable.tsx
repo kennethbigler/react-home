07c89c3058d749f7bd6cc8985a484dbe0387b530
Ken Bigler <Ken_Bigler@intuit.com>
2024-11-26 12:27:55 -0800

    fix: removed a few more props

--- src/components/games/family-feud/FastMoneyTable.tsx
+++ src/components/games/family-feud/FastMoneyTable.tsx
@@ -20,56 +20,58 @@ interface FastMoneyProps {
   ) => React.MouseEventHandler;
 }
 
-const FastMoney = (props: FastMoneyProps) => {
-  const { answerIndex, answers, scores, questions, takenAnswerIndex, onClick } =
-    props;
-
-  return (
-    <TableContainer component={Paper}>
-      <Table aria-label="simple table">
-        <TableBody>
-          {scores.map((score, i) => (
-            <TableRow
-              key={i}
-              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-            >
-              {questions && <TableCell>{questions[i]}</TableCell>}
-              <TableCell component="th" scope="row">
-                {answers[i].map((ans, j) =>
-                  !ans ||
-                  (answerIndex[i] !== -2 && answerIndex[i] !== j) ||
-                  (takenAnswerIndex && takenAnswerIndex[i] === j) ? null : (
-                    <Button
-                      variant={answerIndex[i] === j ? "contained" : "outlined"}
-                      key={j}
-                      onClick={onClick(i, j)}
-                      title={ans}
-                    >
-                      {answerIndex[i] === j ? ans : j + 1}
-                    </Button>
-                  ),
-                )}
-                {answerIndex[i] < 0 && (
+const FastMoney = ({
+  answerIndex,
+  answers,
+  scores,
+  questions,
+  takenAnswerIndex,
+  onClick,
+}: FastMoneyProps) => (
+  <TableContainer component={Paper}>
+    <Table aria-label="simple table">
+      <TableBody>
+        {scores.map((score, i) => (
+          <TableRow
+            key={i}
+            sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+          >
+            {questions && <TableCell>{questions[i]}</TableCell>}
+            <TableCell component="th" scope="row">
+              {answers[i].map((ans, j) =>
+                !ans ||
+                (answerIndex[i] !== -2 && answerIndex[i] !== j) ||
+                (takenAnswerIndex && takenAnswerIndex[i] === j) ? null : (
                   <Button
-                    variant={answerIndex[i] === -1 ? "contained" : "outlined"}
-                    onClick={onClick(i, -1)}
-                    title="Wrong"
+                    variant={answerIndex[i] === j ? "contained" : "outlined"}
+                    key={j}
+                    onClick={onClick(i, j)}
+                    title={ans}
                   >
-                    0
+                    {answerIndex[i] === j ? ans : j + 1}
                   </Button>
-                )}
-              </TableCell>
-              <TableCell align="right">{score}</TableCell>
-            </TableRow>
-          ))}
-          <TableRow sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
-            <TableCell colSpan={questions ? 2 : 1}>Total</TableCell>
-            <TableCell align="right">{scores.reduce(sum, 0)}</TableCell>
+                ),
+              )}
+              {answerIndex[i] < 0 && (
+                <Button
+                  variant={answerIndex[i] === -1 ? "contained" : "outlined"}
+                  onClick={onClick(i, -1)}
+                  title="Wrong"
+                >
+                  0
+                </Button>
+              )}
+            </TableCell>
+            <TableCell align="right">{score}</TableCell>
           </TableRow>
-        </TableBody>
-      </Table>
-    </TableContainer>
-  );
-};
+        ))}
+        <TableRow sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
+          <TableCell colSpan={questions ? 2 : 1}>Total</TableCell>
+          <TableCell align="right">{scores.reduce(sum, 0)}</TableCell>
+        </TableRow>
+      </TableBody>
+    </Table>
+  </TableContainer>
+);
 
 export default FastMoney;
