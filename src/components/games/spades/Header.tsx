c4503467d6b245f409c95040bee62774f6597e9c
Ken Bigler <kennethbigler@gmail.com>
2025-01-07 17:57:00 -0800

    fix: swapped chip order

--- src/components/games/spades/Header.tsx
+++ src/components/games/spades/Header.tsx
@@ -21,18 +21,18 @@ const Header = ({ initials, overBids, wins1, wins2 }: HeaderProps) => (
     <br aria-hidden />
     <div className="flex-container">
       <Chip
-        avatar={<Avatar>{initials[1] + initials[3]}</Avatar>}
-        color={wins2 >= wins1 ? "success" : "error"}
-        label={wins2}
+        avatar={<Avatar>{initials[0] + initials[2]}</Avatar>}
+        color={wins1 >= wins2 ? "success" : "error"}
+        label={wins1}
       />
       <Chip
         avatar={<Avatar>💰</Avatar>}
         label={`${initials[0]} ${overBids[0]} | ${initials[1]} ${overBids[1]} | ${initials[2]} ${overBids[2]} | ${initials[3]} ${overBids[3]}`}
       />
       <Chip
-        avatar={<Avatar>{initials[0] + initials[2]}</Avatar>}
-        color={wins1 >= wins2 ? "success" : "error"}
-        label={wins1}
+        avatar={<Avatar>{initials[1] + initials[3]}</Avatar>}
+        color={wins2 >= wins1 ? "success" : "error"}
+        label={wins2}
       />
     </div>
   </>
