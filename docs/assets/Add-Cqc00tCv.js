8efd3615df1dc9007c10b7ac9bb20dd97d0f352a
Ken Bigler <kennethbigler@gmail.com>
2025-01-26 12:26:16 -0800

    fix: deploy impass update

---
+++ docs/assets/Add-Cqc00tCv.js
@@ -0,0 +1,2 @@
+import{j as o}from"./index-BYmmaOIz.js";import{c as t}from"./Header-BWQdkmh-.js";const h=t(o.jsx("path",{d:"M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6z"}),"Add");export{h as A};
+//# sourceMappingURL=Add-Cqc00tCv.js.map
--- docs/assets/Remove-Bf794k9d.js
+++
@@ -1,2 +0,0 @@
-import{j as o}from"./index-DDpNChgp.js";import{c as e}from"./Header-BwlxGDoG.js";const r=e(o.jsx("path",{d:"M19 13H5v-2h14z"}),"Remove");export{r as R};
-//# sourceMappingURL=Remove-Bf794k9d.js.map
