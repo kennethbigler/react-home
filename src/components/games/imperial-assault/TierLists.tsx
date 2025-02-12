62a49e7558b9372b0cb72028a9636021619f13d8
Ken Bigler <Ken_Bigler@intuit.com>
2025-02-12 08:36:32 -0800

    fix: combine and improve tier lists

--- src/components/games/imperial-assault/TierLists.tsx
+++ src/components/games/imperial-assault/TierLists.tsx
@@ -1,345 +1,293 @@
-import * as React from "react";
 import Grid from "@mui/material/Grid2";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
-import Typography from "@mui/material/Typography";
 import ExpandableCard from "../../common/expandable-card";
 
 interface DeploymentCards {
   name: string;
-  elite?: boolean;
-  cost: string;
-  hunter?: boolean;
-  rank: string;
+  reg?: string;
+  elite?: string;
+  cost?: string;
+  bh?: boolean;
+  divider?: boolean;
 }
 
 const deploymentCards: DeploymentCards[] = [
-  { name: "Nexu", cost: "4", hunter: true, rank: "S" },
-  { name: "Hired Gun", cost: "4/2", hunter: true, rank: "S" },
+  { name: "S-Tier", divider: true },
+  { name: "Nexu", reg: "S+", elite: "A", cost: "⬜️ 4 🟥 6", bh: true },
+  { name: "Hired Gun", reg: "S", elite: "A", cost: "⬜️ 4/2 🟥 6/3", bh: true },
 
-  { name: "Sentry Droid", elite: true, cost: "10/5", rank: "A" },
-  { name: "Riot Trooper", elite: true, cost: "7/3", rank: "A" },
-  { name: "Jet Trooper", elite: true, cost: "7/4", rank: "A" },
-  { name: "Loth-cat", elite: true, cost: "6/3", hunter: true, rank: "A" },
-  { name: "Dewback Rider", elite: true, cost: "5", rank: "A" },
-  { name: "AT-DP", elite: true, cost: "9", rank: "A" },
-  { name: "Death Trooper", cost: "3", rank: "A" },
-  { name: "Death Trooper", elite: true, cost: "4", rank: "A" },
-  { name: "Stormtrooper", elite: true, cost: "9/3", rank: "A" },
-  { name: "Nexu", elite: true, cost: "6", rank: "A" },
-  { name: "Trandoshan Hunter", cost: "7/3", hunter: true, rank: "A" },
-  { name: "Hired Gun", elite: true, cost: "6/3", hunter: true, rank: "A" },
-  { name: "Jawa Scavenger", cost: "2", hunter: true, rank: "A" },
-  { name: "Jawa Scavenger", elite: true, cost: "3", hunter: true, rank: "A" },
-  { name: "Riot Trooper", cost: "5/2", rank: "A" },
-  {
-    name: "Gamorrean Guard",
-    elite: true,
-    cost: "8/4",
-    hunter: true,
-    rank: "A",
-  },
-  { name: "Heavy Stormtrooper", elite: true, cost: "8/4", rank: "A" },
-
-  { name: "Royal Guard", cost: "8/4", rank: "B" },
-  { name: "Loth-cat", cost: "4/2", hunter: true, rank: "B" },
-  { name: "Jet Trooper", cost: "4/2", rank: "B" },
-  { name: "Weequay Pirate", elite: true, cost: "7/4", hunter: true, rank: "B" },
-  { name: "ISB Infiltrator", elite: true, cost: "7/4", rank: "B" },
-  { name: "Stormtrooper", cost: "6/2", rank: "B" },
-  { name: "Probe Droid", elite: true, cost: "5", rank: "B" },
-  { name: "Weequay Pirate", cost: "5/3", hunter: true, rank: "B" },
-  {
-    name: "Ugnaught Tinkerer",
-    elite: true,
-    cost: "5",
-    hunter: true,
-    rank: "B",
-  },
-  { name: "HK Assassin Droid", cost: "8/4", hunter: true, rank: "B" },
-  { name: "Wing Guard", elite: true, cost: "9/3", hunter: true, rank: "B" },
-  {
-    name: "Clawdite Shapeshifter",
-    elite: true,
-    cost: "6",
-    hunter: true,
-    rank: "B",
-  },
-  { name: "SC2-M Repulsor Tank", elite: true, cost: "10", rank: "B" },
-  { name: "E-Web Engineer", elite: true, cost: "8", rank: "B" },
-
-  { name: "Probe Droid", cost: "3", rank: "C" },
-  { name: "Sentry Droid", cost: "6/3", rank: "C" },
-  { name: "Heavy Stormtrooper", cost: "6/3", rank: "C" },
-  { name: "Wing Guard", cost: "6/2", hunter: true, rank: "C" },
-  { name: "ISB Infiltrator", cost: "5/3", rank: "C" },
-  { name: "Royal Guard", elite: true, cost: "12/6", rank: "C" },
+  { name: "A-Tier", divider: true },
+  { name: "Sentry Droid", reg: "C+", elite: "A+", cost: "⬜️ 6/3 🟥 10/5" },
+  { name: "Riot Trooper", reg: "A-", elite: "A+", cost: "⬜️ 5/2 🟥 7/3" },
+  { name: "Jet Trooper", reg: "B+", elite: "A+", cost: "⬜️ 4/2 🟥 7/4" },
+  { name: "Loth-cat", reg: "B+", elite: "A+", cost: "⬜️ 4/2 🟥 6/3", bh: true },
+  { name: "Dewback Rider", reg: "-", elite: "A+", cost: "🟥 5" },
+  { name: "AT-DP", reg: "-", elite: "A+", cost: "🟥 9" },
+  { name: "Death Trooper", reg: "A", elite: "A", cost: "⬜️ 3 🟥 4" },
+  { name: "Stormtrooper", reg: "B", elite: "A", cost: "⬜️ 6/2 🟥 9/3" },
   {
     name: "Trandoshan Hunter",
-    elite: true,
-    cost: "10/5",
-    hunter: true,
-    rank: "C",
+    reg: "A",
+    elite: "C",
+    cost: "⬜️ 7/3 🟥 10/5",
+    bh: true,
   },
-  { name: "Tusken Raider", elite: true, cost: "7/3", hunter: true, rank: "C" },
-  { name: "Ugnaught Tinkerer", cost: "3", hunter: true, rank: "C" },
-  { name: "Imperial Officer", cost: "2", rank: "C" },
-  { name: "Bantha Rider", elite: true, cost: "9", hunter: true, rank: "C" },
-  { name: "Clawdite Shapeshifter", cost: "4", hunter: true, rank: "C" },
-  { name: "Rancor", elite: true, cost: "10", hunter: true, rank: "C" },
-  { name: "Snowtrooper", cost: "7/2", rank: "C" },
-  { name: "E-Web Engineer", cost: "6", rank: "C" },
+  {
+    name: "Jawa Scavenger",
+    reg: "A-",
+    elite: "A-",
+    cost: "⬜️ 2 🟥 3",
+    bh: true,
+  },
+  {
+    name: "Gamorrean Guard",
+    reg: "D-",
+    elite: "A-",
+    cost: "⬜️ 6/3 🟥 8/4",
+    bh: true,
+  },
+  { name: "Heavy Stormtrooper", reg: "C+", elite: "A-", cost: "⬜️ 6/3 🟥 8/4" },
 
-  { name: "Wampa", elite: true, cost: "8", hunter: true, rank: "D" },
-  { name: "Imperial Officer", elite: true, cost: "5", rank: "D" },
-  { name: "Tusken Raider", cost: "5/2", hunter: true, rank: "D" },
-  { name: "Wampa", cost: "5", hunter: true, rank: "D" },
+  { name: "B-Tier", divider: true },
+  { name: "Royal Guard", reg: "B+", elite: "C", cost: "⬜️ 8/4 🟥 12/6" },
+  {
+    name: "Weequay Pirate",
+    reg: "B",
+    elite: "B+",
+    cost: "⬜️ 5/3 🟥 7/4",
+    bh: true,
+  },
+  { name: "ISB Infiltrator", reg: "C+", elite: "B+", cost: "⬜️ 5/3 🟥 7/4" },
+  { name: "Probe Droid", reg: "C+", elite: "B", cost: "⬜️ 3 🟥 5" },
+  {
+    name: "Ugnaught Tinkerer",
+    reg: "C",
+    elite: "B",
+    cost: "⬜️ 3 🟥 5",
+    bh: true,
+  },
   {
     name: "HK Assassin Droid",
-    elite: true,
-    cost: "11/6",
-    hunter: true,
-    rank: "D",
+    reg: "B",
+    elite: "D",
+    cost: "⬜️ 8/4 🟥 11/6",
+    bh: true,
   },
-  { name: "Snowtrooper", elite: true, cost: "10/3", rank: "D" },
-  { name: "Gamorrean Guard", cost: "6/3", hunter: true, rank: "D" },
-  { name: "AT-ST", elite: true, cost: "14", rank: "D" },
+  {
+    name: "Wing Guard",
+    reg: "C+",
+    elite: "B-",
+    cost: "⬜️ 6/2 🟥 9/3",
+    bh: true,
+  },
+  {
+    name: "Clawdite Shapeshifter",
+    reg: "C-",
+    elite: "B-",
+    cost: "⬜️ 4 🟥 6",
+    bh: true,
+  },
+  { name: "SC2-M Repulsor Tank", reg: "-", elite: "B-", cost: "🟥 10" },
+  { name: "E-Web Engineer", reg: "C-", elite: "B-", cost: "⬜️ 6 🟥 8" },
+
+  { name: "C-Tier", divider: true },
+  {
+    name: "Tusken Raider",
+    reg: "D+",
+    elite: "C",
+    cost: "⬜️ 5/2 🟥 7/3",
+    bh: true,
+  },
+  { name: "Imperial Officer", reg: "C", elite: "D+", cost: "⬜️ 2 🟥 5" },
+  { name: "Bantha Rider", reg: "-", elite: "C-", cost: "🟥 9", bh: true },
+  { name: "Rancor", reg: "-", elite: "C-", cost: "🟥 10", bh: true },
+  { name: "Snowtrooper", reg: "C-", elite: "D", cost: "⬜️ 7/2 🟥 10/3" },
+
+  { name: "D-Tier", divider: true },
+  { name: "Wampa", reg: "D", elite: "D+", cost: "⬜️ 5 🟥 8", bh: true },
+  { name: "AT-ST", reg: "-", elite: "D-", cost: "🟥 14" },
 ];
 
 const TierLists = () => (
   <ExpandableCard title="Tier Lists">
-    <Grid container spacing={2} size={12}>
-      <Grid size={{ xs: 6, sm: 4 }}>
-        <Typography variant="h3" gutterBottom>
-          Rebel Heroes
-        </Typography>
+    <Grid container spacing={4} size={12}>
+      <Grid size={{ xs: 12, sm: 5 }}>
         <Table>
           <TableHead>
             <TableRow>
-              <TableCell>Hero</TableCell>
-              <TableCell>Rating</TableCell>
+              <TableCell>Rebel Hero</TableCell>
+              <TableCell>Imperial Class Deck</TableCell>
+              <TableCell>Rank</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
             <TableRow>
-              <TableCell colSpan={2} align="center">
+              <TableCell colSpan={3} align="center">
                 S-Tier
               </TableCell>
             </TableRow>
             <TableRow>
               <TableCell>Gideon Argus</TableCell>
+              <TableCell>Subversive Tactics</TableCell>
               <TableCell>S+</TableCell>
             </TableRow>
             <TableRow>
               <TableCell>Fenn Signis</TableCell>
+              <TableCell>Military Might</TableCell>
               <TableCell>S</TableCell>
             </TableRow>
             <TableRow>
               <TableCell>Diala Passil</TableCell>
+              <TableCell>Hutt Mercenaries</TableCell>
               <TableCell>S</TableCell>
             </TableRow>
             <TableRow>
               <TableCell>Shyla Varad</TableCell>
+              <TableCell></TableCell>
               <TableCell>S</TableCell>
             </TableRow>
             <TableRow>
               <TableCell>Vinto Hreeda</TableCell>
+              <TableCell></TableCell>
               <TableCell>S-</TableCell>
             </TableRow>
             <TableRow>
-              <TableCell colSpan={2} align="center">
+              <TableCell colSpan={3} align="center">
                 A-Tier
               </TableCell>
             </TableRow>
             <TableRow>
               <TableCell>Mak Eshka&apos;rey</TableCell>
+              <TableCell>Tech Superiority</TableCell>
               <TableCell>A+</TableCell>
             </TableRow>
             <TableRow>
               <TableCell>Drokkatta</TableCell>
+              <TableCell>Nemeses</TableCell>
               <TableCell>A+</TableCell>
             </TableRow>
             <TableRow>
               <TableCell>Varena Talos</TableCell>
+              <TableCell>Power of the Dark Side</TableCell>
               <TableCell>A+</TableCell>
             </TableRow>
             <TableRow>
               <TableCell>Gaarkhan</TableCell>
+              <TableCell></TableCell>
               <TableCell>A</TableCell>
             </TableRow>
             <TableRow>
               <TableCell>Jyn Odan</TableCell>
+              <TableCell></TableCell>
               <TableCell>A</TableCell>
             </TableRow>
             <TableRow>
               <TableCell>MDH-19</TableCell>
+              <TableCell></TableCell>
               <TableCell>A</TableCell>
             </TableRow>
             <TableRow>
               <TableCell>Ko-Tun Feralo</TableCell>
+              <TableCell></TableCell>
               <TableCell>A</TableCell>
             </TableRow>
             <TableRow>
               <TableCell>Onar Koma</TableCell>
+              <TableCell></TableCell>
               <TableCell>A</TableCell>
             </TableRow>
             <TableRow>
               <TableCell>Murne Rin</TableCell>
+              <TableCell></TableCell>
               <TableCell>A</TableCell>
             </TableRow>
             <TableRow>
               <TableCell>Jarrod Kelvin</TableCell>
+              <TableCell></TableCell>
               <TableCell>A-</TableCell>
             </TableRow>
             <TableRow>
               <TableCell>Davith Elso</TableCell>
+              <TableCell></TableCell>
               <TableCell>A-</TableCell>
             </TableRow>
             <TableRow>
-              <TableCell colSpan={2} align="center">
+              <TableCell colSpan={3} align="center">
                 B-Tier
               </TableCell>
             </TableRow>
             <TableRow>
               <TableCell>Loku Kanoloa</TableCell>
+              <TableCell>Imperial Black Ops</TableCell>
               <TableCell>B+</TableCell>
             </TableRow>
             <TableRow>
-              <TableCell colSpan={2} align="center">
+              <TableCell></TableCell>
+              <TableCell>Armored Onslaught</TableCell>
+              <TableCell>B</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell></TableCell>
+              <TableCell>Reactive Defenses</TableCell>
+              <TableCell>B</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell></TableCell>
+              <TableCell>Precision Training</TableCell>
+              <TableCell>B</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell colSpan={3} align="center">
                 C-Tier
               </TableCell>
             </TableRow>
             <TableRow>
               <TableCell>Saska Teft</TableCell>
+              <TableCell>Inspiring Leadership</TableCell>
               <TableCell>C+</TableCell>
             </TableRow>
             <TableRow>
               <TableCell>Biv Bodhrik</TableCell>
+              <TableCell>Overwhelming Oppression</TableCell>
               <TableCell>C+</TableCell>
             </TableRow>
           </TableBody>
         </Table>
       </Grid>
 
-      <Grid size={{ xs: 6, sm: 4 }}>
-        <Typography variant="h3" gutterBottom>
-          Imperial Classes
-        </Typography>
+      <Grid size={{ xs: 12, sm: 7 }}>
         <Table>
           <TableHead>
             <TableRow>
-              <TableCell>Decks</TableCell>
-              <TableCell>Rating</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            <TableRow>
-              <TableCell colSpan={2} align="center">
-                S-Tier
-              </TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>Subversive Tactics</TableCell>
-              <TableCell>S+</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>Military Might</TableCell>
-              <TableCell>S</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>Hutt Mercenaries</TableCell>
-              <TableCell>S</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell colSpan={2} align="center">
-                A-Tier
-              </TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>Tech Superiority</TableCell>
-              <TableCell>A+</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>Nemeses</TableCell>
-              <TableCell>A</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>Power of the Dark Side</TableCell>
-              <TableCell>A</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell colSpan={2} align="center">
-                B-Tier
-              </TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>Imperial Black Ops</TableCell>
-              <TableCell>B+</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>Armored Onslaught</TableCell>
-              <TableCell>B</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>Reactive Defenses</TableCell>
-              <TableCell>B</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>Precision Training</TableCell>
-              <TableCell>B</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell colSpan={2} align="center">
-                C-Tier
-              </TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>Inspiring Leadership</TableCell>
-              <TableCell>C</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>Overwhelming Oppression</TableCell>
-              <TableCell>C</TableCell>
-            </TableRow>
-          </TableBody>
-        </Table>
-      </Grid>
-
-      <Grid size={{ xs: 12, sm: 4 }}>
-        <Typography variant="h3" gutterBottom>
-          Deployments
-        </Typography>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>🥊 🔫</TableCell>
-              <TableCell>Name</TableCell>
+              <TableCell>Deployment</TableCell>
+              <TableCell aria-label="Regular">⬜️</TableCell>
+              <TableCell aria-label="Elite">🟥</TableCell>
               <TableCell>Cost</TableCell>
+              <TableCell>🔫</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-            {deploymentCards.map((card, i) => (
-              <React.Fragment key={i}>
-                {card.rank !== deploymentCards[i - 1]?.rank && (
-                  <TableRow>
-                    <TableCell colSpan={3} align="center">
-                      {card.rank}-Tier
-                    </TableCell>
-                  </TableRow>
-                )}
-                <TableRow>
-                  <TableCell>
-                    {card.elite && "🥊"}
-                    {card.hunter && " 🔫"}
+            {deploymentCards.map((card, i) =>
+              card.divider ? (
+                <TableRow key={i}>
+                  <TableCell colSpan={5} align="center">
+                    {card.name}
                   </TableCell>
-                  <TableCell>{card.name}</TableCell>
-                  <TableCell>{card.cost}</TableCell>
                 </TableRow>
-              </React.Fragment>
-            ))}
+              ) : (
+                <TableRow key={i}>
+                  <TableCell>{card.name}</TableCell>
+                  <TableCell>{card.reg}</TableCell>
+                  <TableCell>{card.elite}</TableCell>
+                  <TableCell>{card.cost}</TableCell>
+                  <TableCell>{card.bh && "🔫"}</TableCell>
+                </TableRow>
+              ),
+            )}
           </TableBody>
         </Table>
       </Grid>
