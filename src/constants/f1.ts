e5419804c631e38bfd0a7525ab8d0e38a85449fd
Ken Bigler <kennethbigler@gmail.com>
2025-09-02 18:23:59 -0700

    feat: f1 consts

---
+++ src/constants/f1.ts
@@ -0,0 +1,90 @@
+const teams = [
+  {
+    id: 0,
+    name: "Mercedes",
+    color: "#00D2BE",
+  },
+  {
+    id: 1,
+    name: "Ferrari",
+    color: "#DC0000",
+  },
+  {
+    id: 2,
+    name: "Red Bull Racing",
+    color: "#1E41FF",
+  },
+  {
+    id: 3,
+    name: "Alpine",
+    color: "#0090FF",
+  },
+  {
+    id: 4,
+    name: "Haas",
+    color: "#FFFFFF",
+  },
+  {
+    id: 5,
+    name: "McLaren",
+    color: "#FF8700",
+  },
+  {
+    id: 6,
+    name: "Aston Martin",
+    color: "#006F62",
+  },
+  {
+    id: 7,
+    name: "Alfa Romeo",
+    color: "#900000",
+  },
+  {
+    id: 8,
+    name: "AlphaTauri",
+    color: "#2B4562",
+  },
+  {
+    id: 9,
+    name: "Williams",
+    color: "#005AFF",
+  },
+  {
+    id: 16,
+    name: "Force India",
+    color: "#F596C8",
+  },
+  {
+    id: 17,
+    name: "Sauber",
+    color: "#9B0000",
+  },
+  {
+    id: 18,
+    name: "Toro Rosso",
+    color: "#4E7C9B",
+  },
+  {
+    id: 13,
+    name: "Renault",
+    color: "#FFF500",
+  },
+  {
+    id: 26,
+    name: "Racing Point",
+    color: "#F596C8",
+  },
+];
+
+const nameChanges = {
+  2019: ["Force India > Racing Point", "Sauber > Alfa Romeo"],
+  2020: ["Toro Rosso > AlphaTauri"],
+  2021: ["Renault > Alpine", "Racing Point > Aston Martin"],
+};
+
+const standings = {
+  2018: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+  2019: [1, 2, 3, 6, 4, 8, 7, 8, 5, 10],
+  2020: [1, 3, 6, 7, 4, 2, 9, 8, 5, 10],
+  2021: [1, 3, 2, 6, 4, 9, 7, 10, 8, 5],
+};
