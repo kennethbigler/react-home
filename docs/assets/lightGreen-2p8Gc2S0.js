60c718e7efadb5ae31d643d088f7e0e8514065cb
Ken Bigler <Ken_Bigler@intuit.com>
2025-09-09 13:53:35 -0400

    feat: deploy F1 page

--- docs/assets/lightGreen-2p8Gc2S0.js
+++
@@ -1,2 +0,0 @@
-const c={400:"#9ccc65",500:"#8bc34a",600:"#7cb342"};export{c as l};
-//# sourceMappingURL=lightGreen-2p8Gc2S0.js.map
---
+++ docs/assets/lightGreen-D8V8X1J-.js
@@ -0,0 +1,2 @@
+const c={300:"#aed581",400:"#9ccc65",500:"#8bc34a",600:"#7cb342"};export{c as l};
+//# sourceMappingURL=lightGreen-D8V8X1J-.js.map
