d4262d88b36755d7f938f073b903e8dc256699cf
Ken Bigler <Ken_Bigler@intuit.com>
2025-09-10 12:33:47 -0400

    feat: scaffolding for tracks

---
+++ src/components/resume/f1/Tracks.tsx
@@ -0,0 +1,20 @@
+import Grid from "@mui/material/Grid";
+
+const Tracks = () => (
+  <Grid container spacing={2} width="100%">
+    <Grid size={{ xs: 12, sm: 1 }}>Track 1</Grid>
+    <Grid size={{ xs: 12, sm: 1 }}>Track 2</Grid>
+    <Grid size={{ xs: 12, sm: 1 }}>Track 3</Grid>
+    <Grid size={{ xs: 12, sm: 1 }}>Track 4</Grid>
+    <Grid size={{ xs: 12, sm: 1 }}>Track 5</Grid>
+    <Grid size={{ xs: 12, sm: 1 }}>Track 6</Grid>
+    <Grid size={{ xs: 12, sm: 1 }}>Track 7</Grid>
+    <Grid size={{ xs: 12, sm: 1 }}>Track 8</Grid>
+    <Grid size={{ xs: 12, sm: 1 }}>Track 9</Grid>
+    <Grid size={{ xs: 12, sm: 1 }}>Track 10</Grid>
+    <Grid size={{ xs: 12, sm: 1 }}>Track 11</Grid>
+    <Grid size={{ xs: 12, sm: 1 }}>Track 12</Grid>
+  </Grid>
+);
+
+export default Tracks;
