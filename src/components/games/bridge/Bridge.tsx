3d5c5fc9a8e6e20c6f0858dfb4ff4b6a2b1465c5
Ken Bigler <Ken_Bigler@intuit.com>
2025-10-15 10:35:45 -0400

    feat: add info popups

--- src/components/games/bridge/Bridge.tsx
+++ src/components/games/bridge/Bridge.tsx
@@ -1,10 +1,13 @@
 import Typography from "@mui/material/Typography";
+import InfoPopup from "../../common/info-popover/InfoPopup";
 
 const Bridge = () => (
-  <div>
+  <div className="flex-container">
     <Typography variant="h2" component="h1">
       🌉
     </Typography>
+    <InfoPopup title="Bid">Bid</InfoPopup>
+    <InfoPopup title="Score">Score</InfoPopup>
   </div>
 );
 
