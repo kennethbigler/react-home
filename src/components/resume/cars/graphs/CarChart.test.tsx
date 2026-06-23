806eb1aae6cae2c55e5726d1f6d3203f1da2edb9
Ken Bigler <kennethbigler@gmail.com>
2026-06-23 14:57:36 -0700

    fix: try to get back some coverage

---
+++ src/components/resume/cars/graphs/CarChart.test.tsx
@@ -0,0 +1,19 @@
+import "../tests/highchartsMocks";
+import { render, screen } from "@testing-library/react";
+import CarChart from "./CarChart";
+import { cars } from "../../../../constants/cars";
+
+describe("resume | cars | graphs | CarChart", () => {
+  it("renders the car data chart", () => {
+    render(<CarChart data={cars} color="white" />);
+
+    expect(screen.getByText("Car Data")).toBeInTheDocument();
+    expect(screen.getByTestId("highcharts-chart")).toBeInTheDocument();
+  });
+
+  it("renders an empty chart when no data is provided", () => {
+    render(<CarChart data={[]} color="black" />);
+
+    expect(screen.getByText("Car Data")).toBeInTheDocument();
+  });
+});
